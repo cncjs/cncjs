c4f4c939e5705e6012782e1c143e71e2bf1a21be
Cheton Wu <cheton@gmail.com>
2015-12-11 21:00:06 +0800

    Update build script

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import runSequence from 'run-sequence';
 
 export default (options) => {
+    gulp.task('b', ['browserify']);
     gulp.task('build', (callback) => {
         runSequence('clean',
             'bower',
