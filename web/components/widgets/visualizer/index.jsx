4fddc872ec66eae0e9b62ed940a71d727123b81a
Cheton Wu <cheton@gmail.com>
2015-12-16 00:47:03 +0800

    Refactor Visualizer widget

--- web/components/widgets/visualizer/index.jsx
+++ web/components/widgets/visualizer/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Visualizer from './visualizer';
+import Visualizer from './Visualizer';
 import { Widget, WidgetHeader, WidgetContent } from '../../widget';
 import './index.css';
 
