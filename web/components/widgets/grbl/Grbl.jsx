f6a644a27d42065fca6f55d5a0cad02c4dab56e5
Cheton Wu <cheton@gmail.com>
2015-12-15 23:06:35 +0800

    Refactor Grbl widget

--- web/components/widgets/grbl/Grbl.jsx
+++ web/components/widgets/grbl/Grbl.jsx
@@ -3,57 +3,10 @@ import i18n from 'i18next';
 import pubsub from 'pubsub-js';
 import React from 'react';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
-import classNames from 'classnames';
-import { Widget, WidgetHeader, WidgetContent } from '../widget';
-import log from '../../lib/log';
-import socket from '../../lib/socket';
-import serialport from '../../lib/serialport';
-import './grbl.css';
-
-//
-// https://github.com/grbl/grbl/wiki/Configuring-Grbl-v0.9
-//
-const MODAL_GROUPS = [
-    { // Motion Mode (Defaults to G0)
-        group: 'motion',
-        modes: ['G0', 'G1', 'G2', 'G3', 'G38.2', 'G38.3', 'G38.4', 'G38.5', 'G80']
-    },
-    { // Coordinate System Select (Defaults to G54)
-        group: 'coordinate',
-        modes: ['G54', 'G55', 'G56', 'G57', 'G58', 'G59']
-    },
-    { // Plane Select (Defaults to G17)
-        group: 'plane',
-        modes: ['G17', 'G18', 'G19']
-    },
-    { // Units Mode (Defaults to G21)
-        group: 'units',
-        modes: ['G20', 'G21']
-    },
-    { // Distance Mode (Defaults to G90)
-        group: 'distance',
-        modes: ['G90', 'G91']
-    },
-    { // Feed Rate Mode (Defaults to G94)
-        group: 'feedrate',
-        modes: ['G93', 'G94']
-    },
-    { // Program Mode (Defaults to M0)
-        group: 'program',
-        modes: ['M0', 'M1', 'M2', 'M30']
-    },
-    { // Spindle State (Defaults to M5)
-        group: 'spindle',
-        modes: ['M3', 'M4', 'M5']
-    },
-    { // Coolant State (Defaults to M9)
-        group: 'coolant',
-        modes: ['M7', 'M8', 'M9']
-    }
-];
-
-class GrblGCodeModes extends React.Component {
-}
+import log from '../../../lib/log';
+import socket from '../../../lib/socket';
+import serialport from '../../../lib/serialport';
+import { MODAL_GROUPS } from './constants';
 
 class Grbl extends React.Component {
     state = {
@@ -266,45 +219,4 @@ class Grbl extends React.Component {
     }
 }
 
-class GrblWidget extends React.Component {
-    state = {
-        isCollapsed: false
-    };
-
-    handleClick(target, val) {
-        if (target === 'toggle') {
-            this.setState({
-                isCollapsed: !!val
-            });
-        }
-    }
-    render() {
-        let width = 360;
-        let title = (
-            <div><i className="glyphicon glyphicon-wrench"></i>{i18n._('Grbl')}</div>
-        );
-        let toolbarButtons = [
-            'toggle'
-        ];
-        let widgetContentClass = classNames(
-            { 'hidden': this.state.isCollapsed }
-        );
-
-        return (
-            <div data-component="Widgets/GrblWidget">
-                <Widget width={width}>
-                    <WidgetHeader
-                        title={title}
-                        toolbarButtons={toolbarButtons}
-                        handleClick={::this.handleClick}
-                    />
-                    <WidgetContent className={widgetContentClass}>
-                        <Grbl />
-                    </WidgetContent>
-                </Widget>
-            </div>
-        );
-    }
-}
-
-export default GrblWidget;
+export default Grbl;
---
+++ web/components/widgets/grbl/index.jsx
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import classNames from 'classnames';
+import i18n from 'i18next';
+import React from 'react';
+import Grbl from './Grbl';
+import { Widget, WidgetHeader, WidgetContent } from '../../widget';
+import './index.css';
+
+class GrblWidget extends React.Component {
+    state = {
+        isCollapsed: false
+    };
+
+    handleClick(target, val) {
+        if (target === 'toggle') {
+            this.setState({
+                isCollapsed: !!val
+            });
+        }
+    }
+    render() {
+        let width = 360;
+        let title = (
+            <div><i className="glyphicon glyphicon-wrench"></i>{i18n._('Grbl')}</div>
+        );
+        let toolbarButtons = [
+            'toggle'
+        ];
+        let widgetContentClass = classNames(
+            { 'hidden': this.state.isCollapsed }
+        );
+
+        return (
+            <div data-component="Widgets/GrblWidget">
+                <Widget width={width}>
+                    <WidgetHeader
+                        title={title}
+                        toolbarButtons={toolbarButtons}
+                        handleClick={::this.handleClick}
+                    />
+                    <WidgetContent className={widgetContentClass}>
+                        <Grbl />
+                    </WidgetContent>
+                </Widget>
+            </div>
+        );
+    }
+}
+
+export default GrblWidget;
