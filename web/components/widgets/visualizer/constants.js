1844283eb4d471b3145a5857bb967eee5b8c75e0
Cheton Wu <cheton@gmail.com>
2015-12-16 17:14:06 +0800

    Update constants

--- web/components/widgets/visualizer/constants.js
+++ web/components/widgets/visualizer/constants.js
@@ -1,7 +1,10 @@
-export const CAMERA_DEFAULT_FOV = 50;
-export const CAMERA_DEFAULT_POSITION_X = 0;
-export const CAMERA_DEFAULT_POSITION_Y = 0;
-export const CAMERA_DEFAULT_POSITION_Z = 200; // Move the camera out a bit from the origin (0, 0, 0)
+export const CAMERA_FOV = 50;
+export const CAMERA_ASPECT = 1;
+export const CAMERA_NEAR = 0.1;
+export const CAMERA_FAR = 10000;
+export const CAMERA_POSITION_X = 0;
+export const CAMERA_POSITION_Y = 0;
+export const CAMERA_POSITION_Z = 200; // Move the camera out a bit from the origin (0, 0, 0)
 export const COORDINATE_AXIS_LENGTH = 99999;
 export const WORKFLOW_STATE_RUNNING = 'running';
 export const WORKFLOW_STATE_PAUSED = 'paused';
