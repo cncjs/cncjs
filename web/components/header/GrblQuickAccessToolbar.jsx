b8e219b5f2ff7b05abdcab77a5c8ea8974862526
Cheton Wu <cheton@gmail.com>
2015-12-17 14:27:28 +0800

    Grbl quick access toolbar icons

--- web/components/header/GrblQuickAccessToolbar.jsx
+++ web/components/header/GrblQuickAccessToolbar.jsx
@@ -22,11 +22,13 @@ class GrblQuickAccessToolbar extends React.Component {
         return (
             <div className="grbl-quick-access-toolbar">
                 <div className="btn-group btn-group-sm" role="group">
-                    <button type="button" className="btn btn-default" onClick={::this.handleGrblHelp}>{i18n._('Grbl Help ($)')}</button>
-                    <button type="button" className="btn btn-primary" onClick={::this.handleGrblSettings}>{i18n._('Grbl Settings ($$)')}</button>
-                    <button type="button" className="btn btn-success" onClick={::this.handleReset}>{i18n._('Reset (Ctrl-X)')}</button>
-                    <button type="button" className="btn btn-warning" onClick={::this.handleUnlock}>{i18n._('Unlock ($X)')}</button>
-                    <button type="button" className="btn btn-danger" onClick={::this.handleHome}>{i18n._('Home ($H)')}</button>
+                    <button type="button" className="btn btn-default" onClick={::this.handleGrblHelp}>{i18n._('Grbl Help')}</button>
+                    <button type="button" className="btn btn-default" onClick={::this.handleGrblSettings}>{i18n._('Grbl Settings')}</button>
+                </div>
+                <div className="btn-group btn-group-sm" role="group">
+                    <button type="button" className="btn btn-success" onClick={::this.handleHome}><i className="ion-home"></i>&nbsp;{i18n._('Home')}</button>
+                    <button type="button" className="btn btn-warning" onClick={::this.handleUnlock}><i className="ion-unlocked"></i>&nbsp;{i18n._('Unlock')}</button>
+                    <button type="button" className="btn btn-danger" onClick={::this.handleReset}><i className="ion-reset"></i>&nbsp;{i18n._('Reset')}</button>
                 </div>
             </div>
         );
