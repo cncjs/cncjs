02a460ded591af24fe74d69929b8d1a04ebba7e6
Cheton Wu <cheton@gmail.com>
2015-12-17 23:18:59 +0800

    Adds coordinate grid

--- web/components/widgets/visualizer/constants.js
+++ web/components/widgets/visualizer/constants.js
@@ -1,3 +1,13 @@
+export const COORDINATE_PLANE_XY = 'XY';
+export const COORDINATE_PLANE_XZ = 'XZ';
+export const COORDINATE_PLANE_YZ = 'YZ';
+export const IMPERIAL_AXIS_LINE_LENGTH = 1270;
+export const IMPERIAL_GRID_LINE_LENGTH = 1270;
+export const IMPERIAL_GRID_SPACING = 25.4;
+export const METRIC_AXIS_LINE_LENGTH = 1200;
+export const METRIC_GRID_LINE_LENGTH = 1200;
+export const METRIC_GRID_SPACING = 10;
+
 export const CAMERA_FOV = 50;
 export const CAMERA_ASPECT = 1;
 export const CAMERA_NEAR = 0.1;
@@ -5,7 +15,6 @@ export const CAMERA_FAR = 10000;
 export const CAMERA_POSITION_X = 0;
 export const CAMERA_POSITION_Y = 0;
 export const CAMERA_POSITION_Z = 200; // Move the camera out a bit from the origin (0, 0, 0)
-export const COORDINATE_AXIS_LENGTH = 99999;
 export const WORKFLOW_STATE_RUNNING = 'running';
 export const WORKFLOW_STATE_PAUSED = 'paused';
 export const WORKFLOW_STATE_IDLE = 'idle';
