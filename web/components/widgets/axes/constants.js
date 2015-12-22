31c8947e940f8d9e91a59a5d34c8d904649442f9
Cheton Wu <cheton@gmail.com>
2015-12-22 23:41:15 +0800

    Removes feedrate settings

--- web/components/widgets/axes/constants.js
+++ web/components/widgets/axes/constants.js
@@ -1,15 +1,9 @@
 export const IMPERIAL_UNIT = 'inch';
 export const METRIC_UNIT = 'mm';
 
-// mm/min (or inch/min)
-export const FEEDRATE_MIN = 0;
-export const FEEDRATE_MAX = 1000;
-export const FEEDRATE_STEP = 10;
-export const FEEDRATE_DEFAULT = 250;
-
 // mm (or inch)
 export const DISTANCE_MIN = 0;
-export const DISTANCE_MAX = 1000;
+export const DISTANCE_MAX = 10000;
 export const DISTANCE_STEP = 0.1;
 export const DISTANCE_DEFAULT = 1.00;
 
