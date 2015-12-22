b8ecb2119851f326d5e0bcd2837179a32ae2c473
Cheton Wu <cheton@gmail.com>
2015-12-23 00:17:16 +0800

    Bug fixes

--- web/components/header/GrblQuickAccessToolbar.jsx
+++ web/components/header/GrblQuickAccessToolbar.jsx
@@ -16,9 +16,9 @@ class GrblQuickAccessToolbar extends React.Component {
         return (
             <div className="grbl-quick-access-toolbar">
                 <div className="btn-group btn-group-sm" role="group">
-                    <button type="button" className="btn btn-success" onClick={::this.handleHome} title={i18n._('Run homing cycle ($H)')}><i className="glyphicon glyphicon-home"></i>&nbsp;{i18n._('Home')}</button>
+                    <button type="button" className="btn btn-success" onClick={::this.handleHome} title={i18n._('Run homing cycle ($H)')}><i className="ion-home"></i>&nbsp;{i18n._('Home')}</button>
                     <button type="button" className="btn btn-warning" onClick={::this.handleUnlock} title={i18n._('Kill alarm lock ($X)')}><i className="ion-unlocked"></i>&nbsp;{i18n._('Unlock')}</button>
-                    <button type="button" className="btn btn-danger" onClick={::this.handleReset} title={i18n._('Reset Grbl (Ctrl-X)')}><i className="glyphicon glyphicon-reset"></i>&nbsp;{i18n._('Reset')}</button>
+                    <button type="button" className="btn btn-danger" onClick={::this.handleReset} title={i18n._('Reset Grbl (Ctrl-X)')}><i className="ion-refresh mirror"></i>&nbsp;{i18n._('Reset')}</button>
                 </div>
             </div>
         );
