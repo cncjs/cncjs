a8eb1b6f859ea09928bf766aff54ab2e700854a7
Cheton Wu <cheton@gmail.com>
2015-12-23 20:40:09 +0800

    Adds 'Cycle Start' and 'Feed Hold' button to the Grbl Quick Access Toolbar

--- web/components/header/GrblQuickAccessToolbar.jsx
+++ web/components/header/GrblQuickAccessToolbar.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 import serialport from '../../lib/serialport';
 
 class GrblQuickAccessToolbar extends React.Component {
+    handleCycleStart() {
+        serialport.write('~');
+    }
+    handleFeedHold() {
+        serialport.write('!');
+    }
     handleReset() {
         serialport.write('\x18');
     }
@@ -16,9 +22,13 @@ class GrblQuickAccessToolbar extends React.Component {
         return (
             <div className="grbl-quick-access-toolbar">
                 <div className="btn-group btn-group-sm" role="group">
-                    <button type="button" className="btn btn-success" onClick={::this.handleHome} title={i18n._('Run homing cycle ($H)')}><i className="ion-home"></i>&nbsp;{i18n._('Home')}</button>
-                    <button type="button" className="btn btn-warning" onClick={::this.handleUnlock} title={i18n._('Kill alarm lock ($X)')}><i className="ion-unlocked"></i>&nbsp;{i18n._('Unlock')}</button>
-                    <button type="button" className="btn btn-danger" onClick={::this.handleReset} title={i18n._('Reset Grbl (Ctrl-X)')}><i className="ion-refresh mirror"></i>&nbsp;{i18n._('Reset')}</button>
+                    <button type="button" className="btn btn-default" onClick={::this.handleCycleStart} title={i18n._('Cycle start (~)')}><i className="ion-loop"></i>{i18n._('Cycle Start')}</button>
+                    <button type="button" className="btn btn-default" onClick={::this.handleFeedHold} title={i18n._('Feed hold (!)')}><i className="ion-pause"></i>{i18n._('Feed Hold')}</button>
+                </div>
+                <div className="btn-group btn-group-sm" role="group">
+                    <button type="button" className="btn btn-primary" onClick={::this.handleHome} title={i18n._('Run homing cycle ($H)')}><i className="ion-home"></i>{i18n._('Home')}</button>
+                    <button type="button" className="btn btn-warning" onClick={::this.handleUnlock} title={i18n._('Kill alarm lock ($X)')}><i className="ion-unlocked"></i>{i18n._('Unlock')}</button>
+                    <button type="button" className="btn btn-danger" onClick={::this.handleReset} title={i18n._('Reset Grbl (Ctrl-X)')}><i className="ion-refresh mirror"></i>{i18n._('Reset')}</button>
                 </div>
             </div>
         );
