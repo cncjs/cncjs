e9658bdfca2f7642bbc1c5653b7324fc67e43f89
Cheton Wu (RD-TW) <cheton_wu@trend.com.tw>
2015-12-30 00:45:34 -0800

    Probe widget

--- web/components/widgets/index.js
+++ web/components/widgets/index.js
@@ -2,16 +2,18 @@ import AxesWidget from './axes';
 import ConnectionWidget from './connection';
 import ConsoleWidget from './console';
 import GCodeWidget from './gcode';
-import VisualizerWidget from './visualizer';
 import GrblWidget from './grbl';
+import ProbeWidget from './probe';
 import SpindleWidget from './spindle';
+import VisualizerWidget from './visualizer';
 
 export {
     AxesWidget,
     ConnectionWidget,
     ConsoleWidget,
     GCodeWidget,
-    VisualizerWidget,
     GrblWidget,
-    SpindleWidget
+    ProbeWidget,
+    SpindleWidget,
+    VisualizerWidget
 };
