8ca1343fcf2ac7571aa5f1cc55882e277c715aaa
Cheton Wu <cheton@gmail.com>
2016-01-04 18:06:59 +0800

    socketio-file-upload is no longer needed

--- web/lib/siofu.js
+++
@@ -1,9 +0,0 @@
-import socket from './socket';
-import SocketIOFileUpload from 'socketio-file-upload';
-
-let siofu = new SocketIOFileUpload(socket);
-
-// https://github.com/vote539/socketio-file-upload#instanceusetext--false
-siofu.useText = true;
-
-export default siofu;
