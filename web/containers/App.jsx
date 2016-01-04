226916d590107f9607bc5f0b41235fd5612c3364
Cheton Wu <cheton@gmail.com>
2016-01-04 18:08:07 +0800

    Update styles

--- web/containers/App.jsx
+++ web/containers/App.jsx
@@ -3,12 +3,8 @@ import Header from '../components/header';
 
 class App extends React.Component {
     render() {
-        let style = {
-            paddingTop: '50px',
-            paddingBottom: '20px'
-        };
         return (
-            <div style={style}>
+            <div>
                 <Header />
                 {this.props.children}
             </div>
