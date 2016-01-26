7eff154d62f539b4ab7c23478518ec80b1ac63e0
Cheton Wu <cheton@gmail.com>
2016-01-26 23:00:44 +0800

    ImmutableStore

--- src/web/store/index.js
+++ src/web/store/index.js
@@ -1,75 +1,67 @@
 import _ from 'lodash';
-
-let state;
-
-// Data Migration from v0.14.5
-const migrateState = (userState) => {
-    let probeWidget = _.get(userState, 'widgets.probe');
-
-    // probeDepth
-    let probeDepth = _.get(userState, 'widgets.probe.probeDepth');
-    if (_.isObject(probeDepth)) {
-        let value = probeDepth.mm || probeDepth.in;
-        delete probeWidget.probeDepth.mm;
-        delete probeWidget.probeDepth.in;
-        _.set(probeWidget, 'probeDepth', value);
-    }
-
-    // probeFeedrate
-    let probeFeedrate = _.get(userState, 'widgets.probe.probeFeedrate');
-    if (_.isObject(probeFeedrate)) {
-        let value = probeFeedrate.mm || probeFeedrate.in;
-        delete probeWidget.probeFeedrate.mm;
-        delete probeWidget.probeFeedrate.in;
-        _.set(probeWidget, 'probeFeedrate', value);
-    }
-
-    // tlo
-    let tlo = _.get(userState, 'widgets.probe.tlo');
-    if (_.isObject(tlo)) {
-        let value = tlo.mm || tlo.in;
-        delete probeWidget.tlo.mm;
-        delete probeWidget.tlo.in;
-        _.set(probeWidget, 'tlo', value);
-    }
-
-    // retractionDistance
-    let retractionDistance = _.get(userState, 'widgets.probe.retractionDistance');
-    if (_.isObject(retractionDistance)) {
-        let value = retractionDistance.mm || retractionDistance.in;
-        delete probeWidget.retractionDistance.mm;
-        delete probeWidget.retractionDistance.in;
-        _.set(probeWidget, 'retractionDistance', value);
-    }
-};
+import ImmutableStore from '../lib/immutable-store';
 
 const defaultState = {
     widgets: {
-        connection: {
-            port: '',
-            baudrate: 115200,
-            autoReconnect: true
-        },
         axes: {
+            state: {
+                visibility: 'visible'
+            },
             jog: {
                 selectedDistance: '1',
                 customDistance: 10
             }
         },
+        connection: {
+            state: {
+                visibility: 'visible'
+            },
+            port: '',
+            baudrate: 115200,
+            autoReconnect: true
+        },
+        console: {
+            state: {
+                visibility: 'visible'
+            }
+        },
+        gcode: {
+            state: {
+                visibility: 'visible'
+            }
+        },
+        grbl: {
+            state: {
+                visibility: 'visible'
+            }
+        },
         probe: {
+            state: {
+                visibility: 'visible'
+            },
             probeCommand: 'G38.2',
             probeDepth: 10,
             probeFeedrate: 20,
             tlo: 10,
             retractionDistance: 4
+        },
+        spindle: {
+            state: {
+                visibility: 'visible'
+            }
+        },
+        visualizer: {
+            state: {
+                visibility: 'visible'
+            }
         }
     }
 };
 
+let state;
+
 try {
     let userState = JSON.parse(localStorage.getItem('state') || {});
-
-    migrateState(userState); // for v0.14.5
 
     state = _.merge({}, defaultState, userState);
 }
@@ -77,23 +69,10 @@ catch(err) {
     state = _.merge({}, defaultState);
 }
 
-const setState = (key, value) => {
-    let result = _.set(state, key, value);
+const store = new ImmutableStore(state);
+
+store.on('change', (state) => {
     localStorage.setItem('state', JSON.stringify(state));
-    return result;
-};
+});
 
-const getState = (key, defaultValue) => {
-    let value = _.get(state, key);
-    return (typeof value !== 'undefined') ? value : defaultValue;
-};
-
-const clearState = () => {
-    localStorage.setItem('state', JSON.stringify({}));
-};
-
-export default {
-    setState: setState,
-    getState: getState,
-    clearState: clearState
-};
+export default store;
