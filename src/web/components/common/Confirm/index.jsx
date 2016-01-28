5a179e3f8a9f61a299e9628bf9735b7d76bec46b
Cheton Wu <cheton@gmail.com>
2016-01-29 00:08:42 +0800

    Update Confirm component

--- src/web/components/common/Confirm/index.jsx
+++ src/web/components/common/Confirm/index.jsx
@@ -6,27 +6,33 @@ class Confirm extends React.Component {
     static propTypes = {
         message: React.PropTypes.string,
         description: React.PropTypes.string,
-        onConfirm: React.PropTypes.func,
-        onCancel: React.PropTypes.func,
         confirmLabel: React.PropTypes.string,
-        cancelLabel: React.PropTypes.string
+        cancelLabel: React.PropTypes.string,
+        onConfirm: React.PropTypes.func.isRequired,
+        onCancel: React.PropTypes.func.isRequired,
+        onClose: React.PropTypes.func.isRequired
     };
     state = {
         show: true
     };
 
+    componentDidUpdate() {
+        if (!(this.state.show)) {
+            this.props.onClose();
+        }
+    }
     handleConfirm() {
-        this.props.onConfirm && this.props.onConfirm();
+        this.props.onConfirm();
         this.setState({ show: false });
     }
     handleCancel() {
-        this.props.onCancel && this.props.onCancel();
+        this.props.onCancel();
         this.setState({ show: false });
     }
     render() {
         const {
-            message = i18n._('Are you sure?'),
-            description = '',
+            message,
+            description,
             confirmLabel = i18n._('OK'),
             cancelLabel = i18n._('Cancel')
         } = this.props;
