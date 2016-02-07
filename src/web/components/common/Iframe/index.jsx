60883a6b240d2da0d4b56a21439582c84c1e9e2a
Cheton Wu <cheton@gmail.com>
2016-02-07 16:01:12 +0800

    React Iframe component

---
+++ src/web/components/common/Iframe/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Iframe extends React.Component {
+    static propTypes = {
+        url: React.PropTypes.string.isRequired,
+        width: React.PropTypes.string,
+        height: React.PropTypes.string
+    };
+    static defaultProps = {
+        width: '100%',
+        height: '100%'
+    };
+
+    reload() {
+        let el = ReactDOM.findDOMNode(this);
+        el.src = this.props.url;
+    }
+    render() {
+        const { url, width, height, ...others } = this.props;
+        const style = {
+            overflow: 'hidden',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0
+        };
+
+        return (
+            <iframe
+                {...others}
+                frameBorder="0"
+                src={url}
+                style={style}
+                width={width}
+                height={height}
+            />
+        );
+    }
+}
+
+export default Iframe;
