f4c19c9ad2fcaeeacb123329cef661fc5f77abf4
Cheton Wu <cheton@gmail.com>
2016-02-14 17:19:05 +0800

    Update store

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,30 +1,10 @@
-import _ from 'lodash';
-import events from 'events';
+import ImmutableStore from '../lib/immutable-store';
 
-const defaults = {
+const defaultState = {
     controllers: {}
 };
 
-class ImmutableStore extends events.EventEmitter {
-    state = defaults;
-
-    get(key, defaultValue) {
-        return _.get(this.state, key, defaultValue);
-    }
-    set(key, value) {
-        this.state = _.merge({}, this.state, _.set({}, key, value));
-        this.emit('change', this.state);
-        return this.state;
-    }
-    unset(key) {
-        let state = _.extend({}, this.state);
-        _.unset(state, key);
-        this.state = state;
-        this.emit('change', this.state);
-        return this.state;
-    }
-}
-
-const store = new ImmutableStore();
+const state = _.merge({}, defaultState);
+const store = new ImmutableStore(state);
 
 export default store;
