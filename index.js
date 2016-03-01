c8c2c3299fbb3bae2283178afff20a7a37661184
Cheton Wu <cheton@gmail.com>
2016-03-01 15:07:46 +0800

    Add a mount point option for serving static files

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-var path = require('path');
 var webappengine = require('webappengine');
-var settings = require('./dist/app/config/settings');
 var cncserver = require('./dist/app/cncserver');
-var options = {
-    port: settings.port,
-    routes: [
-        {
-            type: 'server',
-            route: '/',
-            // An absolute path is recommended to use
-            server: path.resolve(__dirname, 'dist/app/app')
-        }
-    ]
-};
 
-webappengine(options)
-    .on('ready', cncserver);
+module.exports = function(options) {
+    webappengine(options)
+        .on('ready', cncserver);
+};
