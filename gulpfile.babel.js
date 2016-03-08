054516a71638117019629e7fc12d19a1f7e4fc76
Cheton Wu <cheton@gmail.com>
2016-03-08 14:25:43 +0800

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,39 +1,45 @@
 import _ from 'lodash';
-import fs from 'fs';
 import gulp from 'gulp';
 import requireDir from 'require-dir';
-import pkg from './package.json';
-import bower from './bower.json';
-import errorHandler from './gulp/error-handler';
+import runSequence from 'run-sequence';
 
-// Sync the following properties from `package.json` to `bower.json`:
-// * name
-// * description
-// * version
-bower.name = pkg.name;
-bower.description = pkg.description;
-bower.version = pkg.version;
-fs.writeFileSync('bower.json', JSON.stringify(bower, null, 2));
+const loadGulpTasks = () => {
+    // Require all tasks in gulp/tasks, including subfolders
+    const tasks = requireDir('./gulp/tasks', { recurse: true });
 
-// Require all tasks in gulp/tasks, including subfolders
-let tasks = requireDir('./gulp/tasks', { recurse: true });
+    // Get environment, for environment-specific activities
+    const env = process.env.NODE_ENV || 'development';
 
-// Get environment, for environment-specific activities
-let env = process.env.NODE_ENV || 'development';
+    _.each(tasks, (task, relativePath) => {
+        if (_.isObject(task) && _.isFunction(task.default)) {
+            task = task.default;
+        }
 
-_.each(tasks, (task, relativePath) => {
-    if (_.isObject(task) && _.isFunction(task.default)) {
-        task = task.default;
-    }
+        console.assert(_.isFunction(task),
+            'gulp/tasks/%s: module\'s export is not a function', relativePath);
 
-    console.assert(_.isFunction(task), 'gulp/tasks/%s: module\'s export is not a function', relativePath);
-
-    task({
-        env: env,
-        watch: false,
-        errorHandler: errorHandler
+        task({ env: env, watch: false });
     });
-});
+};
+
+loadGulpTasks();
 
 gulp.task('default', ['build']);
 gulp.task('dev', ['build-dev']);
+
+gulp.task('build-dev', (callback) => {
+    runSequence(
+        ['app:build', 'web:build-dev'],
+        callback
+    );
+});
+
+gulp.task('build', (callback) => {
+    runSequence('clean',
+        ['bower', 'eslint', 'jshint'],
+        ['app:build', 'web:build'],
+        ['app:i18n', 'web:i18n'],
+        ['app:dist', 'web:dist'],
+        callback
+    );
+});
