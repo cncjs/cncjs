0eb2eb5f5cf198ff5fd8a6f38ae715e1feb4de84
Cheton Wu <cheton@gmail.com>
2016-03-16 23:26:46 +0800

    webpack plugins

--- webpack.config.app.js
+++ webpack.config.app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var path = require('path');
+var webpack = require('webpack');
 
 // http://jlongster.com/Backend-Apps-with-Webpack--Part-I
 var nodeModules = {};
@@ -24,6 +25,15 @@ module.exports = {
         filename: '[name].js',
         libraryTarget: 'commonjs2'
     },
+    plugins: [
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    ],
     module: {
         loaders: [
             {
