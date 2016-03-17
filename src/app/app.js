6ac4858130af1f0e202237f1259e8c880edab029
Cheton Wu <cheton@gmail.com>
2016-03-17 21:28:51 +0800

    Use the publicPath from webpack.config.development.js

--- src/app/app.js
+++ src/app/app.js
@@ -92,13 +92,11 @@ const webpackMain = (app) => {
         noInfo: false,
         quite: false,
         lazy: false,
-        
         // https://webpack.github.io/docs/node.js-api.html#compiler
         watchOptions: {
             poll: true // use polling instead of native watchers
         },
-
-        publicPath: '/',
+        publicPath: config.output.publicPath,
         stats: {
             colors: true
         }
