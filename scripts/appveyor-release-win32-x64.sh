94be99e302afb86ed04ac880643fc62abce905bb
Cheton Wu <cheton@gmail.com>
2016-04-28 11:22:24 +0800

    Use commit logs as description

--- scripts/appveyor-release-win32-x64.sh
+++ scripts/appveyor-release-win32-x64.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 FILE=$1
-RELEASE="cncjs-${APPVEYOR_TAG_NAME:-${APPVEYOR_REPO_BRANCH:-latest}}"
-
 if [[ -z "$FILE" ]]; then
     echo "The file is not specified."
     exit 1;
 fi
 
+RELEASE="cncjs-${APPVEYOR_TAG_NAME:-${APPVEYOR_REPO_BRANCH:-latest}}"
+DESCRIPTION=`git log -1 --date=iso`
+
 scripts/github-release.sh \
     --user="cheton" \
     --repo="cnc-builds" \
     --tag="$RELEASE" \
-    --description="AppVeyor build: $APPVEYOR_BUILD_VERSION" \
+    --description="$DESCRIPTION" \
     --name="${RELEASE}-win32-x64.zip" \
     --file="$FILE"
--- scripts/travis-release-linux-x64.sh
+++ scripts/travis-release-linux-x64.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 FILE=$1
-RELEASE="cncjs-${TRAVIS_TAG:-${TRAVIS_BRANCH:-latest}}"
-
 if [[ -z "$FILE" ]]; then
     echo "The file is not specified."
     exit 1;
 fi
 
+RELEASE="cncjs-${TRAVIS_TAG:-${TRAVIS_BRANCH:-latest}}"
+DESCRIPTION=`git log -1 --date=iso`
+
 scripts/github-release.sh \
     --user="cheton" \
     --repo="cnc-builds" \
     --tag="$RELEASE" \
-    --description="Travis build: ${TRAVIS_BUILD_NUMBER}" \
+    --description="$DESCRIPTION" \
     --name="${RELEASE}-linux-x64.zip" \
     --file="$FILE"
