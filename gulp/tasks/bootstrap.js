2cc4674675d66ea92002f997802bcd5f80840cef
Cheton Wu <cheton@gmail.com>
2016-04-28 23:04:41 +0800

    Do not sync description

--- gulp/tasks/bootstrap.js
+++ gulp/tasks/bootstrap.js
@@ -17,10 +17,14 @@ export default (options) => {
         // Copy necessary properties from 'package.json' to 'src/package.json'
         const _pkg = require('../../src/package.json');
 
-        // Development package.json: name=cncjs (https://www.npmjs.com/package/cncjs)
-        // Application package.json: name=cnc
+        // Development:
+        //   package.json
+        //   - name: cncjs (https://www.npmjs.com/package/cncjs)
+        //
+        // Application:
+        //   src/package.json
+        //   - name: cnc
         _pkg.version = pkg.version;
-        _pkg.description = pkg.description;
         _pkg.homepage = pkg.homepage;
         _pkg.author = pkg.author;
         _pkg.license = pkg.license;
