673f565f3fe7458eaff813f0fd4bd9e906f1c8f1
Cheton Wu <cheton@gmail.com>
2016-05-03 18:47:18 +0800

    Add pkg-sync task

---
+++ gulp/tasks/pkg-sync.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import pkg from '../../package.json';
+
+export default (options) => {
+    gulp.task('pkg-sync', (callback) => {
+        // Copy necessary properties from 'package.json' to 'src/package.json'
+        const _pkg = require('../../src/package.json');
+
+        // Development:
+        //   package.json
+        //   - name: cncjs (https://www.npmjs.com/package/cncjs)
+        //
+        // Application:
+        //   src/package.json
+        //   - name: cnc
+        _pkg.version = pkg.version;
+        _pkg.homepage = pkg.homepage;
+        _pkg.author = pkg.author;
+        _pkg.license = pkg.license;
+        _pkg.dependencies = pkg.dependencies;
+
+        const target = path.resolve(__dirname, '../../src/package.json');
+        const content = JSON.stringify(_pkg, null, 2);
+        fs.writeFileSync(target, content + '\n', 'utf8');
+
+        return gulp.src('src/package.json')
+            .pipe(gulp.dest('dist/cnc'));
+    });
+};
