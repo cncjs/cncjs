932dc6ecf5b7fce831d46e8c0054027798007423
Cheton Wu <cheton@gmail.com>
2016-05-09 18:31:08 +0800

    Use the abbreviated commit hash instead of the 'latest' string

--- scripts/prebuild-latest.sh
+++ scripts/prebuild-latest.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+abbrev_commit=`git log -1 --format=%h`
+
 gulp pkg-sync
 
 pushd src
-npm version $npm_package_version-latest
+npm version $npm_package_version-$abbrev_commit
 mkdir -p ../dist/cnc/
 cp -af package.json ../dist/cnc/
 babel -d ../dist/cnc/ *.js
