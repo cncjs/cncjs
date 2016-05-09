49a18849d9026d2861e5c4f6d186476dfcd2e1b4
Cheton Wu <cheton@gmail.com>
2016-05-09 16:59:55 +0800

    Copy only Node.js dependencies to application package.json

--- gulp/tasks/pkg-sync.js
+++ gulp/tasks/pkg-sync.js
@@ -1,7 +1,58 @@
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
+import glob from 'glob';
 import gulp from 'gulp';
 import pkg from '../../package.json';
+
+const findDeps = (patterns) => {
+    let filenames = [];
+
+    patterns.forEach((pattern) => {
+        filenames = filenames.concat(glob.sync(pattern));
+    });
+
+    let deps = [];
+    _.each(filenames, (pathname) => {
+        const content = fs.readFileSync(pathname, 'utf8');
+
+        { // import
+            let r = null;
+            const patternImport = new RegExp(/[^0-9a-zA-Z_$\n]*import\s+("[^"]+"|'[^']+'|\{[^\}]+\}|[a-zA-Z_$][0-9a-zA-Z_$]*|(?:\*\s+as\s+[a-zA-Z_$][0-9a-zA-Z_$]*\s*,?\s*)*)\s*(?:from\s*((?:"[^"]+")|(?:'[^']+')))?/gm);
+            while ((r = patternImport.exec(content))) {
+                const dep = (r[2] || r[1] || '')
+                    .replace(/^"(.+)"$/, '$1')
+                    .replace(/^'(.+)'$/, '$1');
+
+                if (dep.startsWith('.') || dep.startsWith('/')) {
+                    continue;
+                }
+
+                deps = _.union(deps, [dep]);
+            }
+        }
+
+        { // require
+            let r = null;
+            const patternRequire = new RegExp(/[^a-zA-Z0-9_\n]*require\(("[^"]+"|'[^']+'|[^"']+)\)/gm);
+            while ((r = patternRequire.exec(content))) {
+                const dep = (r[1] || '')
+                    .replace(/^"(.+)"$/, '$1')
+                    .replace(/^'(.+)'$/, '$1');
+
+                if (dep.startsWith('.') || dep.startsWith('/')) {
+                    continue;
+                }
+
+                deps = _.union(deps, [dep]);
+            }
+        }
+    });
+
+    deps = deps.sort();
+
+    return deps;
+};
 
 export default (options) => {
     gulp.task('pkg-sync', (callback) => {
@@ -19,7 +70,12 @@ export default (options) => {
         _pkg.homepage = pkg.homepage;
         _pkg.author = pkg.author;
         _pkg.license = pkg.license;
-        _pkg.dependencies = pkg.dependencies;
+
+        // Copy only Node.js dependencies to application package.json
+        _pkg.dependencies = _.pick(pkg.dependencies, findDeps([
+            'src/*.js',
+            'src/app/**/*.{js,jsx}',
+        ]));
 
         const target = path.resolve(__dirname, '../../src/package.json');
         const content = JSON.stringify(_pkg, null, 2);
