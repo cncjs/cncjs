afc21859eee4b06aefcd8efdea2462ffdbd7f2c8
Cheton Wu <cheton@gmail.com>
2016-06-25 11:43:25 +0800

    Add keyboard shortcuts for jogging

--- src/web/lib/combokeys.js
+++ src/web/lib/combokeys.js
@@ -2,125 +2,405 @@ import events from 'events';
 import Mousetrap from 'mousetrap';
 import log from './log';
 
+const AXIS_X = 'x';
+const AXIS_Y = 'y';
+const AXIS_Z = 'z';
+const FORWARD = 1;
+const BACKWARD = -1;
+const OVERSHOOT_FACTOR = 5; // 1x5=5, 0.1x5=0.5, 0.01x5=0.05
+const UNDERSHOOT_FACTOR = 0.5; // 1/2=0.5, 0.1/2=0.05, 0.01/2=0.005
+
 const commandKeys = [
     { // Feed Hold
-        cmd: 'FEED_HOLD',
-        keys: '!'
+        keys: '!',
+        cmd: 'CONTROLLER_COMMAND',
+        payload: {
+            command: 'feedhold'
+        },
+        preventDefault: true
     },
-    { // Resume
-        cmd: 'RESUME',
-        keys: '~'
+    { // Cycle Start
+        keys: '~',
+        cmd: 'CONTROLLER_COMMAND',
+        payload: {
+            command: 'cyclestart'
+        },
+        preventDefault: true
     },
     { // Homing
-        cmd: 'HOMING',
-        keys: ['ctrl', 'alt', 'command', 'h'].join('+')
+        keys: ['ctrl', 'alt', 'command', 'h'].join('+'),
+        cmd: 'CONTROLLER_COMMAND',
+        payload: {
+            command: 'homing'
+        },
+        preventDefault: true
     },
     { // Unlock
-        cmd: 'UNLOCK',
-        keys: ['ctrl', 'alt', 'command', 'u'].join('+')
+        keys: ['ctrl', 'alt', 'command', 'u'].join('+'),
+        cmd: 'CONTROLLER_COMMAND',
+        payload: {
+            command: 'unlock'
+        },
+        preventDefault: true
     },
     { // Reset
-        cmd: 'RESET',
-        keys: ['ctrl', 'alt', 'command', 'r'].join('+')
+        keys: ['ctrl', 'alt', 'command', 'r'].join('+'),
+        cmd: 'CONTROLLER_COMMAND',
+        payload: {
+            command: 'reset'
+        },
+        preventDefault: true
     },
     { // Jog Lever Switch
+        keys: ['ctrl', 'alt', 'command', '='].join('+'),
         cmd: 'JOG_LEVER_SWITCH',
-        keys: ['ctrl', 'alt', 'command', '='].join('+')
+        payload: {
+        },
+        preventDefault: true
     },
-    { // Jog Forward
-        cmd: 'JOG_FORWARD',
-        keys: ['ctrl', 'alt', 'command', ']'].join('+')
+    { // Jog X+/Y+/Z+ (selected axis)
+        keys: ['ctrl', 'alt', 'command', ']'].join('+'),
+        cmd: 'JOG',
+        payload: {
+            axis: null,
+            direction: FORWARD,
+            factor: 1
+        },
+        preventDefault: true
     },
-    { // Jog Backward
-        cmd: 'JOG_BACKWARD',
-        keys: ['ctrl', 'alt', 'command', '['].join('+')
+    { // Jog X-/Y-/Z- (selected axis)
+        keys: ['ctrl', 'alt', 'command', '['].join('+'),
+        cmd: 'JOG',
+        payload: {
+            axis: null,
+            direction: BACKWARD,
+            factor: 1
+        },
+        preventDefault: true
+    },
+    { // Jog X+
+        keys: 'right',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_X,
+            direction: FORWARD,
+            factor: 1
+        },
+        preventDefault: false
+    },
+    { // Jog X+ (undershoot)
+        keys: 'alt+right',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_X,
+            direction: FORWARD,
+            factor: UNDERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog X+ (overshoot)
+        keys: 'shift+right',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_X,
+            direction: FORWARD,
+            factor: OVERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog X-
+        keys: 'left',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_X,
+            direction: BACKWARD,
+            factor: 1
+        },
+        preventDefault: false
+    },
+    { // Jog X- (undershoot)
+        keys: 'alt+left',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_X,
+            direction: BACKWARD,
+            factor: UNDERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog X- (overshoot)
+        keys: 'shift+left',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_X,
+            direction: BACKWARD,
+            factor: OVERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Y+
+        keys: 'up',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Y,
+            direction: FORWARD,
+            factor: 1
+        },
+        preventDefault: false
+    },
+    { // Jog Y+ (undershoot)
+        keys: 'alt+up',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Y,
+            direction: FORWARD,
+            factor: UNDERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Y+ (overshoot)
+        keys: 'shift+up',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Y,
+            direction: FORWARD,
+            factor: OVERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Y-
+        keys: 'down',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Y,
+            direction: BACKWARD,
+            factor: 1
+        },
+        preventDefault: false
+    },
+    { // Jog Y- (undershoot)
+        keys: 'alt+down',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Y,
+            direction: BACKWARD,
+            factor: UNDERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Y- (overshoot)
+        keys: 'shift+down',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Y,
+            direction: BACKWARD,
+            factor: OVERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Z+
+        keys: 'pageup',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Z,
+            direction: FORWARD,
+            factor: 1
+        },
+        preventDefault: false
+    },
+    { // Jog Z+ (undershoot)
+        keys: 'alt+pageup',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Z,
+            direction: FORWARD,
+            factor: UNDERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Z+ (overshoot)
+        keys: 'shift+pageup',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Z,
+            direction: FORWARD,
+            factor: OVERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Z-
+        keys: 'pagedown',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Z,
+            direction: BACKWARD,
+            factor: 1
+        },
+        preventDefault: false
+    },
+    { // Jog Z- (undershoot)
+        keys: 'alt+pagedown',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Z,
+            direction: BACKWARD,
+            factor: UNDERSHOOT_FACTOR
+        },
+        preventDefault: false
+    },
+    { // Jog Z- (overshoot)
+        keys: 'shift+pagedown',
+        cmd: 'JOG',
+        payload: {
+            axis: AXIS_Z,
+            direction: BACKWARD,
+            factor: OVERSHOOT_FACTOR
+        },
+        preventDefault: false
     },
     { // Shuttle Zone -7
-        cmd: 'SHUTTLE_ZONE',
-        value: -7,
-        keys: ['ctrl', 'alt', 'shift', '7'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '7'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -7
+        },
+        preventDefault: true
     },
     { // Shuttle Zone -6
-        cmd: 'SHUTTLE_ZONE',
-        value: -6,
-        keys: ['ctrl', 'alt', 'shift', '6'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '6'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -6
+        },
+        preventDefault: true
     },
     { // Shuttle Zone -5
-        cmd: 'SHUTTLE_ZONE',
-        value: -5,
-        keys: ['ctrl', 'alt', 'shift', '5'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '5'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -5
+        },
+        preventDefault: true
     },
     { // Shuttle Zone -4
-        cmd: 'SHUTTLE_ZONE',
-        value: -4,
-        keys: ['ctrl', 'alt', 'shift', '4'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '4'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -4
+        },
+        preventDefault: true
     },
     { // Shuttle Zone -3
-        cmd: 'SHUTTLE_ZONE',
-        value: -3,
-        keys: ['ctrl', 'alt', 'shift', '3'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '3'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -3
+        },
+        preventDefault: true
     },
     { // Shuttle Zone -2
-        cmd: 'SHUTTLE_ZONE',
-        value: -2,
-        keys: ['ctrl', 'alt', 'shift', '2'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '2'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -2
+        },
+        preventDefault: true
     },
     { // Shuttle Zone -1
-        cmd: 'SHUTTLE_ZONE',
-        value: -1,
-        keys: ['ctrl', 'alt', 'shift', '1'].join('+')
+        keys: ['ctrl', 'alt', 'shift', '1'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: -1
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 0
-        cmd: 'SHUTTLE_ZONE',
-        value: 0,
-        keys: ['ctrl', 'alt', 'command', '0'].join('+')
+        keys: ['ctrl', 'alt', 'command', '0'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 0
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 1
-        cmd: 'SHUTTLE_ZONE',
-        value: 1,
-        keys: ['ctrl', 'alt', 'command', '1'].join('+')
+        keys: ['ctrl', 'alt', 'command', '1'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 1
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 2
-        cmd: 'SHUTTLE_ZONE',
-        value: 2,
-        keys: ['ctrl', 'alt', 'command', '2'].join('+')
+        keys: ['ctrl', 'alt', 'command', '2'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 2
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 3
-        cmd: 'SHUTTLE_ZONE',
-        value: 3,
-        keys: ['ctrl', 'alt', 'command', '3'].join('+')
+        keys: ['ctrl', 'alt', 'command', '3'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 3
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 4
-        cmd: 'SHUTTLE_ZONE',
-        value: 4,
-        keys: ['ctrl', 'alt', 'command', '4'].join('+')
+        keys: ['ctrl', 'alt', 'command', '4'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 4
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 5
-        cmd: 'SHUTTLE_ZONE',
-        value: 5,
-        keys: ['ctrl', 'alt', 'command', '5'].join('+')
+        keys: ['ctrl', 'alt', 'command', '5'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 5
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 6
-        cmd: 'SHUTTLE_ZONE',
-        value: 6,
-        keys: ['ctrl', 'alt', 'command', '6'].join('+')
+        keys: ['ctrl', 'alt', 'command', '6'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 6
+        },
+        preventDefault: true
     },
     { // Shuttle Zone 7
-        cmd: 'SHUTTLE_ZONE',
-        value: 7,
-        keys: ['ctrl', 'alt', 'command', '7'].join('+')
+        keys: ['ctrl', 'alt', 'command', '7'].join('+'),
+        cmd: 'SHUTTLE',
+        payload: {
+            value: 7
+        },
+        preventDefault: true
     },
-    { // X-Axis
-        cmd: 'X_AXIS',
-        keys: ['ctrl', 'alt', 'command', 'x'].join('+')
+    { // Select X-axis
+        keys: ['ctrl', 'alt', 'command', 'x'].join('+'),
+        cmd: 'SELECT_AXIS',
+        payload: {
+            axis: 'x'
+        },
+        preventDefault: true
     },
-    { // Y-Axis
-        cmd: 'Y_AXIS',
-        keys: ['ctrl', 'alt', 'command', 'y'].join('+')
+    { // Select Y-axis
+        keys: ['ctrl', 'alt', 'command', 'y'].join('+'),
+        cmd: 'SELECT_AXIS',
+        payload: {
+            axis: 'y'
+        },
+        preventDefault: true
     },
-    { // Z-Axis
-        cmd: 'Z_AXIS',
-        keys: ['ctrl', 'alt', 'command', 'z'].join('+')
+    { // Select Z-axis
+        keys: ['ctrl', 'alt', 'command', 'z'].join('+'),
+        cmd: 'SELECT_AXIS',
+        payload: {
+            axis: 'z'
+        },
+        preventDefault: true
     }
 ];
 
@@ -142,10 +422,10 @@ class Combokeys extends events.EventEmitter {
             return;
         }
         commandKeys.forEach((o) => {
-            const { keys, cmd, value } = o;
-            const callback = () => {
-                log.debug('combokeys:', keys, cmd, value);
-                this.emit(cmd, value);
+            const { cmd, payload = {}, keys } = o;
+            const callback = (event) => {
+                log.debug(`combokeys: cmd=${cmd} payload=${JSON.stringify(payload)} keys=${keys}`);
+                this.emit(cmd, event, payload);
             };
             Mousetrap.bind(keys, callback);
             this.list.push({ keys: keys, callback: callback });
