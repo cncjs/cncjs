dc05afd8aa80d9371daf73dd22a402bdcbeab85f
Cheton Wu <cheton@gmail.com>
2016-07-03 20:14:01 +0800

    Add support for editing current work position and issue the `G10 L20 Px` command (#54)

--- src/web/components/widgets/axes/DisplayPanel.jsx
+++ src/web/components/widgets/axes/DisplayPanel.jsx
@@ -1,9 +1,12 @@
 import { isEqual } from 'lodash';
 import React, { Component, PropTypes } from 'react';
 import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { in2mm } from '../../../lib/units';
 import i18n from '../../../lib/i18n';
 import controller from '../../../lib/controller';
+import PositionInput from './PositionInput';
 import {
+    IMPERIAL_UNIT,
     METRIC_UNIT
 } from './constants';
 
@@ -13,8 +16,14 @@ class DisplayPanel extends Component {
         actions: PropTypes.object
     };
 
+    state = {
+        showXPositionInput: false,
+        showYPositionInput: false,
+        showZPositionInput: false
+    };
+
     shouldComponentUpdate(nextProps, nextState) {
-        return !isEqual(nextProps, this.props);
+        return !isEqual(nextProps, this.props) || !isEqual(nextState, this.state);
     }
     handleSelect(eventKey) {
         const data = eventKey;
@@ -48,10 +57,34 @@ class DisplayPanel extends Component {
                                 <span className="dimension-unit">{displayUnit}</span>
                             </td>
                             <td className="work-position">
-                                <span className="integer-part">{workPosition.x.split('.')[0]}</span>
-                                <span className="decimal-point">.</span>
-                                <span className="fractional-part">{workPosition.x.split('.')[1]}</span>
                                 <span className="dimension-unit">{displayUnit}</span>
+                            {this.state.showXPositionInput &&
+                                <PositionInput
+                                    onOK={(value) => {
+                                        if (unit === IMPERIAL_UNIT) {
+                                            value = in2mm(value);
+                                        }
+                                        controller.writeln('G10 L20 P1 X' + value);
+                                        this.setState({ showXPositionInput: false });
+                                    }}
+                                    onCancel={() => {
+                                        this.setState({ showXPositionInput: false });
+                                    }}
+                                />
+                            }
+                            {!this.state.showXPositionInput &&
+                                <div
+                                    style={{ cursor: 'pointer' }}
+                                    title={i18n._('Edit')}
+                                    onClick={() => {
+                                        this.setState({ showXPositionInput: true });
+                                    }}
+                                >
+                                    <span className="integer-part">{workPosition.x.split('.')[0]}</span>
+                                    <span className="decimal-point">.</span>
+                                    <span className="fractional-part">{workPosition.x.split('.')[1]}</span>
+                                </div>
+                            }
                             </td>
                             <td className="action">
                                 <DropdownButton
@@ -114,10 +147,34 @@ class DisplayPanel extends Component {
                                 <span className="dimension-unit">{displayUnit}</span>
                             </td>
                             <td className="work-position">
-                                <span className="integer-part">{workPosition.y.split('.')[0]}</span>
-                                <span className="decimal-point">.</span>
-                                <span className="fractional-part">{workPosition.y.split('.')[1]}</span>
                                 <span className="dimension-unit">{displayUnit}</span>
+                            {this.state.showYPositionInput &&
+                                <PositionInput
+                                    onOK={(value) => {
+                                        if (unit === IMPERIAL_UNIT) {
+                                            value = in2mm(value);
+                                        }
+                                        controller.writeln('G10 L20 P1 Y' + value);
+                                        this.setState({ showYPositionInput: false });
+                                    }}
+                                    onCancel={() => {
+                                        this.setState({ showYPositionInput: false });
+                                    }}
+                                />
+                            }
+                            {!this.state.showYPositionInput &&
+                                <div
+                                    style={{ cursor: 'pointer' }}
+                                    title={i18n._('Edit')}
+                                    onClick={() => {
+                                        this.setState({ showYPositionInput: true });
+                                    }}
+                                >
+                                    <span className="integer-part">{workPosition.y.split('.')[0]}</span>
+                                    <span className="decimal-point">.</span>
+                                    <span className="fractional-part">{workPosition.y.split('.')[1]}</span>
+                                </div>
+                            }
                             </td>
                             <td className="action">
                                 <DropdownButton
@@ -180,10 +237,34 @@ class DisplayPanel extends Component {
                                 <span className="dimension-unit">{displayUnit}</span>
                             </td>
                             <td className="work-position">
-                                <span className="integer-part">{workPosition.z.split('.')[0]}</span>
-                                <span className="decimal-point">.</span>
-                                <span className="fractional-part">{workPosition.z.split('.')[1]}</span>
                                 <span className="dimension-unit">{displayUnit}</span>
+                            {this.state.showZPositionInput &&
+                                <PositionInput
+                                    onOK={(value) => {
+                                        if (unit === IMPERIAL_UNIT) {
+                                            value = in2mm(value);
+                                        }
+                                        controller.writeln('G10 L20 P1 Z' + value);
+                                        this.setState({ showZPositionInput: false });
+                                    }}
+                                    onCancel={() => {
+                                        this.setState({ showZPositionInput: false });
+                                    }}
+                                />
+                            }
+                            {!this.state.showZPositionInput &&
+                                <div
+                                    style={{ cursor: 'pointer' }}
+                                    title={i18n._('Edit')}
+                                    onClick={() => {
+                                        this.setState({ showZPositionInput: true });
+                                    }}
+                                >
+                                    <span className="integer-part">{workPosition.z.split('.')[0]}</span>
+                                    <span className="decimal-point">.</span>
+                                    <span className="fractional-part">{workPosition.z.split('.')[1]}</span>
+                                </div>
+                            }
                             </td>
                             <td className="action">
                                 <DropdownButton
---
+++ src/web/components/widgets/axes/PositionInput.jsx
@@ -0,0 +1,85 @@
+import React, { Component, PropTypes } from 'react';
+
+class PositionInput extends Component {
+    static propTypes = {
+        defaultValue: PropTypes.string,
+        onOK: PropTypes.func.isRequired,
+        onCancel: PropTypes.func.isRequired,
+        min: PropTypes.number,
+        max: PropTypes.number
+    };
+    static defaultProps = {
+        defaultValue: '',
+        onOK: (value) => {},
+        onCancel: () => {},
+        min: -10000,
+        max: 10000
+    };
+
+    state = {
+        value: this.props.defaultValue
+    };
+
+    componentDidMount() {
+        this.refs.positionInput.focus();
+    }
+    render() {
+        const { onOK, onCancel, min, max } = this.props;
+        const isNumber = (this.state.value !== '');
+
+        return (
+            <div className="input-group input-group-xs" style={{ width: '100%' }}>
+                <input
+                    ref="positionInput"
+                    type="number"
+                    className="form-control"
+                    placeholder=""
+                    style={{ borderRight: 'none' }}
+                    value={this.state.value}
+                    onChange={(event) => {
+                        let value = event.target.value;
+
+                        if (value === '') {
+                            this.setState({ value: '' });
+                            return;
+                        }
+                        if (value >= min && value <= max) {
+                            this.setState({ value: value });
+                        }
+                    }}
+                    onKeyDown={(event) => {
+                        if (event.keyCode === 13) { // ENTER
+                            onOK(this.state.value);
+                        }
+                        if (event.keyCode === 27) { // ESC
+                            onCancel();
+                        }
+                    }}
+                />
+                <div className="input-group-btn">
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        disabled={!isNumber}
+                        onClick={(event) => {
+                            onOK(this.state.value);
+                        }}
+                    >
+                        <i className="fa fa-check" />
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        onClick={(event) => {
+                            onCancel();
+                        }}
+                    >
+                        <i className="fa fa-close" />
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default PositionInput;
