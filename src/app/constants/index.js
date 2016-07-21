dc32a3b62ce44738080f74a7c48f455b12243d39
Cheton Wu <cheton@gmail.com>
2016-07-21 23:44:08 +0800

    Update TinyG2 constants

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -17,14 +17,19 @@ export const GRBL_ACTIVE_STATE_ALARM = 'Alarm';
 export const GRBL_ACTIVE_STATE_CHECK = 'Check';
 
 // TinyG2 Machine State
-export const TINYG2_MACHINE_STATE_RESET = 'Reset';
-export const TINYG2_MACHINE_STATE_ALARM = 'Alarm';
-export const TINYG2_MACHINE_STATE_STOP = 'Stop';
-export const TINYG2_MACHINE_STATE_END = 'End';
-export const TINYG2_MACHINE_STATE_RUN = 'Run';
-export const TINYG2_MACHINE_STATE_HOLD = 'Hold';
-export const TINYG2_MACHINE_STATE_PROBE = 'Probe';
-export const TINYG2_MACHINE_STATE_HOMING = 'Homing';
+// https://github.com/synthetos/TinyG/wiki/TinyG-Status-Codes#status-report-enumerations
+export const TINYG2_MACHINE_STATE_INIT = 0; // machine is initializing
+export const TINYG2_MACHINE_STATE_READY = 1; // machine is ready for use
+export const TINYG2_MACHINE_STATE_ALARM = 2; // machine is in alarm state (soft shut down)
+export const TINYG2_MACHINE_STATE_STOP = 3; // program stop or no more blocks (M0, M1, M60)
+export const TINYG2_MACHINE_STATE_END = 4; // program end via M2, M30
+export const TINYG2_MACHINE_STATE_RUN = 5; // motion is running
+export const TINYG2_MACHINE_STATE_HOLD = 6; // motion is holding
+export const TINYG2_MACHINE_STATE_PROBE = 7; // probe cycle active
+export const TINYG2_MACHINE_STATE_CYCLING = 8; // machine is running (cycling)
+export const TINYG2_MACHINE_STATE_HOMING = 9; // machine is homing
+export const TINYG2_MACHINE_STATE_JOGGING = 10; // machine is jogging
+export const TINYG2_MACHINE_STATE_SHUTDOWN = 11; // machine is in hard alarm state (shut down)
 
 // Grbl Real-time Commands: ~, !, ?, and Ctrl-x
 export const GRBL_REALTIME_COMMANDS = [
