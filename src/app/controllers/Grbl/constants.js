95e0720ef448f3a1faef9815eb8f07da44206c92
Cheton Wu <cheton@gmail.com>
2016-07-22 23:16:11 +0800

    Update constant values

---
+++ src/app/controllers/Grbl/constants.js
@@ -0,0 +1,60 @@
+// Grbl
+export const GRBL = 'Grbl';
+
+// Active State
+export const GRBL_ACTIVE_STATE_IDLE = 'Idle';
+export const GRBL_ACTIVE_STATE_RUN = 'Run';
+export const GRBL_ACTIVE_STATE_HOLD = 'Hold';
+export const GRBL_ACTIVE_STATE_DOOR = 'Door';
+export const GRBL_ACTIVE_STATE_HOME = 'Home';
+export const GRBL_ACTIVE_STATE_ALARM = 'Alarm';
+export const GRBL_ACTIVE_STATE_CHECK = 'Check';
+
+// Real-time Commands: ~, !, ?, and Ctrl-x
+export const GRBL_REALTIME_COMMANDS = [
+    '~', // Cycle Start
+    '!', // Feed Hold
+    '?', // Current Status
+    '\x18' // Reset Grbl (Ctrl-X)
+];
+
+// https://github.com/grbl/grbl/wiki/Configuring-Grbl-v0.9
+// http://linuxcnc.org/docs/html/gcode/overview.html#cap:modal-groups
+export const GRBL_MODAL_GROUPS = [
+    { // Motion Mode (Defaults to G0)
+        group: 'motion',
+        modes: ['G0', 'G1', 'G2', 'G3', 'G38.2', 'G38.3', 'G38.4', 'G38.5', 'G80']
+    },
+    { // Coordinate System Select (Defaults to G54)
+        group: 'coordinate',
+        modes: ['G54', 'G55', 'G56', 'G57', 'G58', 'G59']
+    },
+    { // Plane Select (Defaults to G17)
+        group: 'plane',
+        modes: ['G17', 'G18', 'G19']
+    },
+    { // Units Mode (Defaults to G21)
+        group: 'units',
+        modes: ['G20', 'G21']
+    },
+    { // Distance Mode (Defaults to G90)
+        group: 'distance',
+        modes: ['G90', 'G91']
+    },
+    { // Feed Rate Mode (Defaults to G94)
+        group: 'feedrate',
+        modes: ['G93', 'G94']
+    },
+    { // Program Mode (Defaults to M0)
+        group: 'program',
+        modes: ['M0', 'M1', 'M2', 'M30']
+    },
+    { // Spindle State (Defaults to M5)
+        group: 'spindle',
+        modes: ['M3', 'M4', 'M5']
+    },
+    { // Coolant State (Defaults to M9)
+        group: 'coolant',
+        modes: ['M7', 'M8', 'M9']
+    }
+];
---
+++ src/app/controllers/TinyG2/constants.js
@@ -0,0 +1,76 @@
+// TinyG2
+export const TINYG2 = 'TinyG2';
+
+// Machine State
+// https://github.com/synthetos/TinyG/wiki/TinyG-Status-Codes#status-report-enumerations
+export const TINYG2_MACHINE_STATE_INIT = 0; // machine is initializing
+export const TINYG2_MACHINE_STATE_READY = 1; // machine is ready for use
+export const TINYG2_MACHINE_STATE_ALARM = 2; // machine is in alarm state (soft shut down)
+export const TINYG2_MACHINE_STATE_STOP = 3; // program stop or no more blocks (M0, M1, M60)
+export const TINYG2_MACHINE_STATE_END = 4; // program end via M2, M30
+export const TINYG2_MACHINE_STATE_RUN = 5; // motion is running
+export const TINYG2_MACHINE_STATE_HOLD = 6; // motion is holding
+export const TINYG2_MACHINE_STATE_PROBE = 7; // probe cycle active
+export const TINYG2_MACHINE_STATE_CYCLING = 8; // machine is running (cycling)
+export const TINYG2_MACHINE_STATE_HOMING = 9; // machine is homing
+export const TINYG2_MACHINE_STATE_JOGGING = 10; // machine is jogging
+export const TINYG2_MACHINE_STATE_SHUTDOWN = 11; // machine is in hard alarm state (shut down)
+
+// Cycle State
+export const TINYG2_CYCLE_STATE_OFF = 0; // cycle off (not in cycle)
+export const TINYG2_CYCLE_STATE_NORMAL = 1; // normal machine state
+export const TINYG2_CYCLE_STATE_PROBE = 2; // probe cycle
+export const TINYG2_CYCLE_STATE_HOMING = 3; // homing cycle
+export const TINYG2_CYCLE_STATE_JOG = 4; // jog cycle
+
+// Motion State
+export const TINYG2_MOTION_STATE_OFF = 0; // motion off
+export const TINYG2_MOTION_STATE_RUN = 1; // motion run
+export const TINYG2_MOTION_STATE_HOLD = 2; // motion hold
+
+// Feedhold State
+export const TINYG2_FEEDHOLD_STATE_OFF = 0; // feedhold off (not in feedhold)
+export const TINYG2_FEEDHOLD_STATE_SYNC = 1; // feedhold sync phase
+export const TINYG2_FEEDHOLD_STATE_PLANNING = 2; // feedhold planning phase
+export const TINYG2_FEEDHOLD_STATE_DECELERATION = 3; // feedhold deceleration phase
+export const TINYG2_FEEDHOLD_STATE_HOLDING = 4; // feedhold holding
+export const TINYG2_FEEDHOLD_STATE_END = 5; // feedhold end hold
+
+// G-code Motion Mode
+export const TINYG2_GCODE_MOTION_G0 = 0;
+export const TINYG2_GCODE_MOTION_G1 = 1;
+export const TINYG2_GCODE_MOTION_G2 = 2;
+export const TINYG2_GCODE_MOTION_G3 = 3;
+export const TINYG2_GCODE_MOTION_G80 = 4;
+
+// G-code Coordinate System
+export const TINYG2_GCODE_COORDINATE_G53 = 0;
+export const TINYG2_GCODE_COORDINATE_G54 = 1;
+export const TINYG2_GCODE_COORDINATE_G55 = 2;
+export const TINYG2_GCODE_COORDINATE_G56 = 3;
+export const TINYG2_GCODE_COORDINATE_G57 = 4;
+export const TINYG2_GCODE_COORDINATE_G58 = 5;
+export const TINYG2_GCODE_COORDINATE_G59 = 6;
+
+// G-code Plane Selection
+export const TINYG2_GCODE_PLANE_G17 = 0;
+export const TINYG2_GCODE_PLANE_G18 = 1;
+export const TINYG2_GCODE_PLANE_G19 = 2;
+
+// G-code Units
+export const TINYG2_GCODE_UNITS_G20 = 0;
+export const TINYG2_GCODE_UNITS_G21 = 1;
+
+// G-code Distance Mode
+export const TINYG2_GCODE_DISTANCE_G90 = 0;
+export const TINYG2_GCODE_DISTANCE_G91 = 1;
+
+// G-code Feedrate Mode
+export const TINYG2_GCODE_FEEDRATE_G93 = 0;
+export const TINYG2_GCODE_FEEDRATE_G94 = 1;
+export const TINYG2_GCODE_FEEDRATE_G95 = 2;
+
+// G-code Path Control Mode
+export const TINYG2_GCODE_PATH_G61 = 0;
+export const TINYG2_GCODE_PATH_G61_1 = 1;
+export const TINYG2_GCODE_PATH_G64 = 2;
