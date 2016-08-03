154ff224a8583b4613ff962c236c4909ccfdb8d3
Cheton Wu <cheton@gmail.com>
2016-08-03 19:01:20 +0800

    Update Grbl widget

--- src/web/components/widgets/grbl/Grbl.jsx
+++ src/web/components/widgets/grbl/Grbl.jsx
@@ -77,10 +77,10 @@ class Grbl extends Component {
     render() {
         const { state, actions } = this.props;
         const { canClick, showGCode } = state;
+        const none = '–';
         const controllerState = state.controller.state;
         const activeState = _.get(controllerState, 'status.activeState');
         const parserstate = _.get(controllerState, 'parserstate', {});
-        const none = '–';
         let modal = parserstate.modal || {};
 
         if (!showGCode) {
@@ -89,123 +89,121 @@ class Grbl extends Component {
 
         return (
             <div>
-                <Toolbar {...this.props} />
-                <div className="parser-state">
-                    <div className="row no-gutters">
-                        <div className="col col-xs-12">
-                            <h6>{i18n._('Parser State')}</h6>
-                        </div>
-                    </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('State')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{activeState}</div>
-                        </div>
-                        <div className="col col-xs-3">
-                            {i18n._('Feed Rate')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{Number(parserstate.feedrate) || 0}</div>
-                        </div>
-                    </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('Spindle')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{Number(parserstate.spindle) || 0}</div>
-                        </div>
-                        <div className="col col-xs-3">
-                            {i18n._('Tool Number')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{parserstate.tool || none}</div>
-                        </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-12">
+                        <Toolbar {...this.props} />
                     </div>
                 </div>
-                <div className="modal-groups">
-                    <div className="row no-gutters">
-                        <div className="col col-xs-6">
-                            <h6 className="modal-groups-header">
-                                {i18n._('Modal Groups')}
-                            </h6>
-                        </div>
-                        <div className="col col-xs-6 text-right">
-                            <button
-                                type="button"
-                                className="btn btn-xs btn-default btn-toggle-display"
-                                onClick={actions.toggleDisplay}
-                                disabled={!canClick}
-                            >
-                                {i18n._('Toggle Display')}
-                            </button>
-                        </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-12">
+                        <h6>{i18n._('Parser State')}</h6>
                     </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('Motion')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.motion || none}</div>
-                        </div>
-                        <div className="col col-xs-3">
-                            {i18n._('Coordinate')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.coordinate || none}</div>
-                        </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('State')}
                     </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('Plane')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.plane || none}</div>
-                        </div>
-                        <div className="col col-xs-3">
-                            {i18n._('Distance')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.distance || none}</div>
-                        </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{activeState || none}</div>
                     </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('Feed Rate')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.feedrate || none}</div>
-                        </div>
-                        <div className="col col-xs-3">
-                            {i18n._('Units')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.units || none}</div>
-                        </div>
+                    <div className="col col-xs-3">
+                        {i18n._('Feed Rate')}
                     </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('Program')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.program || none}</div>
-                        </div>
-                        <div className="col col-xs-3">
-                            {i18n._('Spindle')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.spindle || none}</div>
-                        </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{Number(parserstate.feedrate) || 0}</div>
                     </div>
-                    <div className="row no-gutters">
-                        <div className="col col-xs-3">
-                            {i18n._('Coolant')}
-                        </div>
-                        <div className="col col-xs-3">
-                            <div className="well well-xs">{modal.coolant || none}</div>
-                        </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('Spindle')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{Number(parserstate.spindle) || 0}</div>
+                    </div>
+                    <div className="col col-xs-3">
+                        {i18n._('Tool Number')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{parserstate.tool || none}</div>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-6">
+                        <h6>{i18n._('Modal Groups')}</h6>
+                    </div>
+                    <div className="col col-xs-6 text-right">
+                        <button
+                            type="button"
+                            className="btn btn-xs btn-default btn-toggle-display"
+                            onClick={actions.toggleDisplay}
+                            disabled={!canClick}
+                        >
+                            {i18n._('Toggle Display')}
+                        </button>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('Motion')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.motion || none}</div>
+                    </div>
+                    <div className="col col-xs-3">
+                        {i18n._('Coordinate')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.coordinate || none}</div>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('Plane')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.plane || none}</div>
+                    </div>
+                    <div className="col col-xs-3">
+                        {i18n._('Distance')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.distance || none}</div>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('Feed Rate')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.feedrate || none}</div>
+                    </div>
+                    <div className="col col-xs-3">
+                        {i18n._('Units')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.units || none}</div>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('Program')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.program || none}</div>
+                    </div>
+                    <div className="col col-xs-3">
+                        {i18n._('Spindle')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.spindle || none}</div>
+                    </div>
+                </div>
+                <div className="row no-gutters">
+                    <div className="col col-xs-3">
+                        {i18n._('Coolant')}
+                    </div>
+                    <div className="col col-xs-3">
+                        <div className="well well-xs">{modal.coolant || none}</div>
                     </div>
                 </div>
             </div>
--- src/web/components/widgets/grbl/Toolbar.jsx
+++ src/web/components/widgets/grbl/Toolbar.jsx
@@ -16,51 +16,84 @@ class Toolbar extends Component {
     render() {
         const { state } = this.props;
         const { canClick } = state;
+        const styles = {
+            button: {
+                minWidth: 24
+            }
+        };
 
         return (
-            <div className="btn-group btn-group-xs">
-                <button
-                    type="button"
-                    className="btn btn-default"
-                    onClick={() => controller.command('cyclestart')}
-                    disabled={!canClick}
-                >
-                    <i className="fa fa-play" /> {i18n._('Cycle Start')}
-                </button>
-                <button
-                    type="button"
-                    className="btn btn-default"
-                    onClick={() => controller.command('feedhold')}
-                    disabled={!canClick}
-                >
-                    <i className="fa fa-pause" /> {i18n._('Feed Hold')}
-                </button>
-                <button
-                    type="button"
-                    className="btn btn-default"
-                    onClick={() => controller.command('reset')}
-                    disabled={!canClick}
-                >
-                    <i className="fa fa-undo" /> {i18n._('Reset')}
-                </button>
-                <DropdownButton
-                    bsSize="xs"
-                    bsStyle="default"
-                    title=""
-                    id="grbl-dropdown"
-                    disabled={!canClick}
-                >
-                    <MenuItem onSelect={() => controller.writeln('$')} disabled={!canClick}>{i18n._('Help ($)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$$')} disabled={!canClick}>{i18n._('Settings ($$)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$#')} disabled={!canClick}>{i18n._('View G-code Parameters ($#)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$G')} disabled={!canClick}>{i18n._('View G-code Parser State ($G)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$I')} disabled={!canClick}>{i18n._('View Build Info ($I)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$N')} disabled={!canClick}>{i18n._('View Startup Blocks ($N)')}</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem onSelect={() => controller.writeln('$C')} disabled={!canClick}>{i18n._('Check G-code Mode ($C)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$X')} disabled={!canClick}>{i18n._('Kill Alarm Lock ($X)')}</MenuItem>
-                    <MenuItem onSelect={() => controller.writeln('$H')} disabled={!canClick}>{i18n._('Run Homing Cycle ($H)')}</MenuItem>
-                </DropdownButton>
+            <div>
+                <div className="btn-group btn-group-xs">
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        title={i18n._('Feedhold')}
+                        style={styles.button}
+                        onClick={() => controller.command('feedhold')}
+                        disabled={!canClick}
+                    >
+                        !
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        title={i18n._('Cycle Start')}
+                        style={styles.button}
+                        onClick={() => controller.command('cyclestart')}
+                        disabled={!canClick}
+                    >
+                        ~
+                    </button>
+                </div>
+                <div className="btn-group btn-group-xs" style={{ marginLeft: 10 }}>
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        title={i18n._('Kill Alarm Lock')}
+                        style={styles.button}
+                        onClick={() => controller.command('unlock')}
+                        disabled={!canClick}
+                    >
+                        $X
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        title={i18n._('Check G-code Mode')}
+                        style={styles.button}
+                        onClick={() => controller.command('check')}
+                        disabled={!canClick}
+                    >
+                        $C
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-default"
+                        title={i18n._('Reset')}
+                        style={styles.button}
+                        onClick={() => controller.command('reset')}
+                        disabled={!canClick}
+                    >
+                        ^x
+                    </button>
+                </div>
+                <div className="btn-group pull-right">
+                    <DropdownButton
+                        bsSize="xs"
+                        bsStyle="default"
+                        title={i18n._('More')}
+                        id="grbl-dropdown"
+                        disabled={!canClick}
+                    >
+                        <MenuItem onSelect={() => controller.writeln('$')} disabled={!canClick}>{i18n._('Help ($)')}</MenuItem>
+                        <MenuItem onSelect={() => controller.writeln('$$')} disabled={!canClick}>{i18n._('Settings ($$)')}</MenuItem>
+                        <MenuItem onSelect={() => controller.writeln('$#')} disabled={!canClick}>{i18n._('View G-code Parameters ($#)')}</MenuItem>
+                        <MenuItem onSelect={() => controller.writeln('$G')} disabled={!canClick}>{i18n._('View G-code Parser State ($G)')}</MenuItem>
+                        <MenuItem onSelect={() => controller.writeln('$I')} disabled={!canClick}>{i18n._('View Build Info ($I)')}</MenuItem>
+                        <MenuItem onSelect={() => controller.writeln('$N')} disabled={!canClick}>{i18n._('View Startup Blocks ($N)')}</MenuItem>
+                    </DropdownButton>
+                </div>
             </div>
         );
     }
