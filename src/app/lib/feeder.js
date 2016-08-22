0466b854838b871c3ea23e9f74d29f723df68115
Cheton Wu <cheton@gmail.com>
2016-08-22 23:07:02 +0800

    Add feeder library

---
+++ src/app/lib/feeder.js
@@ -0,0 +1,31 @@
+import events from 'events';
+
+class Feeder extends events.EventEmitter {
+    queue = [];
+    pending = false;
+
+    feed(data) {
+        this.queue.push(data);
+    }
+    clear() {
+        this.queue = [];
+        this.pending = false;
+    }
+    next() {
+        if (this.queue.length === 0) {
+            this.pending = false;
+            return false;
+        }
+
+        const data = this.queue.shift();
+        this.pending = true;
+        this.emit('data', data);
+
+        return data;
+    }
+    isPending() {
+        return this.pending;
+    }
+}
+
+export default Feeder;
