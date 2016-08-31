9f6c394c78d8e404c855e4528b3f81a0ff9cf7b2
Cheton Wu <cheton@gmail.com>
2016-09-01 00:31:05 +0800

    Remove macro action constants

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -2,7 +2,3 @@
 export const WORKFLOW_STATE_RUNNING = 'running';
 export const WORKFLOW_STATE_PAUSED = 'paused';
 export const WORKFLOW_STATE_IDLE = 'idle';
-
-// Macro action
-export const MACRO_ACTION_START = 'start';
-export const MACRO_ACTION_STOP = 'stop';
