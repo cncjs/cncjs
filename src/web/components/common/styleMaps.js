602dc4f1e1739100b691f734e47c6d6213484d77
Cheton Wu <cheton@gmail.com>
2016-09-01 11:54:08 +0800

    Add styleMaps.js

---
+++ src/web/components/common/styleMaps.js
@@ -0,0 +1,49 @@
+const constant = obj => {
+    return Object.assign(
+        Object.create({
+            values() {
+                return Object.keys(this).map(k => this[k]);
+            }
+        }), obj);
+};
+
+const styleMaps = {
+    SIZES: {
+        'large': 'lg',
+        'medium': 'md',
+        'small': 'sm',
+        'xsmall': 'xs',
+        'lg': 'lg',
+        'md': 'md',
+        'sm': 'sm',
+        'xs': 'xs'
+    },
+    GRID_COLUMNS: 12
+};
+
+export const Sizes = constant({
+    LARGE: 'large',
+    MEDIUM: 'medium',
+    SMALL: 'small',
+    XSMALL: 'xsmall'
+});
+
+export const State = constant({
+    SUCCESS: 'success',
+    WARNING: 'warning',
+    DANGER: 'danger',
+    ERROR: 'error',
+    INFO: 'info'
+});
+
+export const Theme = constant({
+    DARK: 'dark',
+    LIGHT: 'light'
+});
+
+export const DEFAULT = 'default';
+export const PRIMARY = 'primary';
+export const LINK = 'link';
+export const INVERSE = 'inverse';
+
+export default styleMaps;
