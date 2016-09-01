0a0e601c9b1d1a1d2c6d0a2689246cd52b4fd449
Cheton Wu <cheton@gmail.com>
2016-09-01 13:20:12 +0800

    Update error hint

--- src/web/components/widgets/macro/AddMacro.jsx
+++ src/web/components/widgets/macro/AddMacro.jsx
@@ -24,7 +24,7 @@ Object.assign(Validation.rules, {
         },
         hint: (value) => {
             return (
-                <HelpBlock>{i18n._('This field cannot be blank')}</HelpBlock>
+                <HelpBlock>{i18n._('This field is required.')}</HelpBlock>
             );
         }
     }
