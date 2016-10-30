6c7a2c00f0fe7cb43c630e4a31d7b682ac1cb503
Cheton Wu <cheton@gmail.com>
2016-10-30 18:17:27 +0800

    Add numeral.js

---
+++ src/web/lib/numeral.js
@@ -0,0 +1,11 @@
+// http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+export const formatBytes = (bytes, decimals = 3) => {
+    if (!bytes) {
+        return '0 byte';
+    }
+
+    const k = 1000;
+    const sizes = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(decimals)) + ' ' + sizes[i];
+};
