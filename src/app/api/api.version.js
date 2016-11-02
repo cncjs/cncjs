8c090bf85e54f4b01083e4287631f4ffcd5b6ae0
Cheton Wu <cheton@gmail.com>
2016-11-03 00:06:16 +0800

    Return update time for the latest release

--- src/app/api/api.version.js
+++ src/app/api/api.version.js
@@ -1,18 +1,20 @@
 import packageJson from 'package-json';
 
 export const getLatestVersion = (req, res) => {
-    packageJson('cncjs', 'latest')
-        .then(json => {
+    packageJson('cncjs')
+        .then(data => {
+            const latest = data['dist-tags'].latest;
+            const time = data.time[latest];
             const {
                 name,
                 version,
                 description,
                 homepage
-            } = { ...json };
+            } = { ...data.versions[latest] };
 
-            res.send({ name, version, description, homepage });
+            res.send({ time, name, version, description, homepage });
         })
-        .catch((err) => {
+        .catch(err => {
             res.status(400).send({
                 err: err
             });
