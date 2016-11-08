d6fab5c81307a2435a5095fa5abaebc95f17040a
Cheton Wu <cheton@gmail.com>
2016-11-08 18:40:46 +0800

    Move write-file-webpack-plugin from src/app/webpack-dev-server.js to webpack.config.development.js

--- src/app/webpack-dev-server.js
+++ src/app/webpack-dev-server.js
@@ -1,18 +1,10 @@
 import webpack from 'webpack';
-import WriteFileWebpackPlugin from 'write-file-webpack-plugin';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import config from '../../webpack.config.development';
 
 const webpackDevServer = (app) => {
     // https://github.com/webpack/webpack-dev-middleware
-    const compiler = webpack({
-        ...config,
-        plugins: [
-            // https://github.com/gajus/write-file-webpack-plugin
-            // Forces webpack-dev-server to write bundle files to the file system.
-            new WriteFileWebpackPlugin()
-        ].concat(config.plugins)
-    });
+    const compiler = webpack(config);
 
     // https://github.com/webpack/webpack-dev-middleware
     // webpack-dev-middleware handle the files in memory.
