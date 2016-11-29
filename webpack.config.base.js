235c1375abc8859256adcab81b9f9431be903ff5
Cheton Wu <cheton@gmail.com>
2016-11-30 07:49:41 +0800

    Allow removing relative paths when importing

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -27,7 +27,7 @@ module.exports = {
             // http://survivejs.com/webpack_react/linting_in_webpack/
             {
                 test: /\.jsx?$/,
-                loaders: ['eslint'],
+                loader: 'eslint',
                 exclude: /node_modules/
             },
             {
@@ -98,7 +98,10 @@ module.exports = {
     },
     resolve: {
         alias: {},
-        extensions: ['', '.js', '.jsx', '.styl']
+        extensions: ['', '.js', '.jsx', '.styl'],
+        root: [
+            path.resolve(__dirname, 'src/web')
+        ]
     },
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
