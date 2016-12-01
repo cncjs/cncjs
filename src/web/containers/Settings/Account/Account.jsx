6981e9e705afb522b69843bf2352cc1720f9d262
Cheton Wu <cheton@gmail.com>
2016-12-01 15:11:29 +0800

    Code comment updates

--- src/web/containers/Settings/Account/Account.jsx
+++ src/web/containers/Settings/Account/Account.jsx
@@ -176,7 +176,7 @@ class Account extends Component {
                                                             actions.fetchData();
                                                         })
                                                         .catch((res) => {
-                                                            // TODO
+                                                            // Ignore error
                                                         });
                                                 });
                                             }}
