f928572af9d125443828a0149615b87d8b9ffab2
Cheton Wu <cheton@gmail.com>
2017-04-04 14:35:51 +0800

    Use the registry API to get latest package version from the npm registry

--- src/app/api/api.version.js
+++ src/app/api/api.version.js
@@ -1,10 +1,35 @@
-import packageJson from 'package-json';
+import url from 'url';
+import registryUrl from 'registry-url';
+import registryAuthToken from 'registry-auth-token';
+import request from 'superagent';
+import pkg from '../../package.json';
 
 export const getLatestVersion = (req, res) => {
-    packageJson('cncjs')
-        .then(data => {
-            const latest = data['dist-tags'].latest;
+    const scope = pkg.name.split('/')[0];
+    const regUrl = registryUrl(scope);
+    const pkgUrl = url.resolve(regUrl, encodeURIComponent(pkg.name).replace(/^%40/, '@'));
+    const authInfo = registryAuthToken(regUrl);
+    const headers = {};
+
+    if (authInfo) {
+        headers.Authorization = `${authInfo.type} ${authInfo.token}`;
+    }
+
+    request
+        .get(pkgUrl)
+        .set(headers)
+        .end((err, { body: data = {} }) => {
+            if (err) {
+                res.status(err.statusCode).send({ err: err });
+                return;
+            }
+
+            data.time = data.time || {};
+            data['dist-tags'] = data['dist-tags'] || {};
+            data.versions = data.versions || {};
+
             const time = data.time[latest];
+            const latest = data['dist-tags'].latest;
             const {
                 name,
                 version,
@@ -13,10 +38,5 @@ export const getLatestVersion = (req, res) => {
             } = { ...data.versions[latest] };
 
             res.send({ time, name, version, description, homepage });
-        })
-        .catch(err => {
-            res.status(400).send({
-                err: err
-            });
         });
 };
