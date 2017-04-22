cb84c1614be86a28e118602b9395160319c6bbd8
Cheton Wu <cheton@gmail.com>
2017-04-22 13:00:07 +0800

    Fix a bug of uncaught exception caused by incorrect status code

--- src/app/api/api.version.js
+++ src/app/api/api.version.js
@@ -3,6 +3,9 @@ import registryUrl from 'registry-url';
 import registryAuthToken from 'registry-auth-token';
 import request from 'superagent';
 import pkg from '../../package.json';
+import {
+    ERR_INTERNAL_SERVER_ERROR
+} from '../constants';
 
 export const getLatestVersion = (req, res) => {
     const scope = pkg.name.split('/')[0];
@@ -20,7 +23,9 @@ export const getLatestVersion = (req, res) => {
         .set(headers)
         .end((err, _res) => {
             if (err) {
-                res.status(err.statusCode).send({ err: err });
+                res.status(ERR_INTERNAL_SERVER_ERROR).send({
+                    msg: `Failed to connect to ${pkgUrl}: code=${err.code}`
+                });
                 return;
             }
 
