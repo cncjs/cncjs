e1e8f518e2414c32f1e5c5b3a6222cf92b6fd668
Cheton Wu <cheton@gmail.com>
2017-08-14 21:53:04 +0800

    Modify request headers and query parameters to prevent caching

--- src/web/api/index.js
+++ src/web/api/index.js
@@ -1,5 +1,6 @@
 import superagent from 'superagent';
 import superagentUse from 'superagent-use';
+import ensureArray from '../lib/ensure-array';
 import store from '../store';
 
 const bearer = (request) => {
@@ -9,8 +10,22 @@ const bearer = (request) => {
     }
 };
 
+// Modify request headers and query parameters to prevent caching
+const noCache = (request) => {
+    const now = Date.now();
+    request.set('Cache-Control', 'no-cache');
+    request.set('X-Requested-With', 'XMLHttpRequest');
+
+    if (request.method === 'GET' || request.method === 'HEAD') {
+        // Force requested pages not to be cached by the browser by appending "_={timestamp}" to the GET parameters, this will work correctly with HEAD and GET requests. The parameter is not needed for other types of requests, except in IE8 when a POST is made to a URL that has already been requested by a GET.
+        request._query = ensureArray(request._query);
+        request._query.push(`_=${now}`);
+    }
+};
+
 const authrequest = superagentUse(superagent);
 authrequest.use(bearer);
+authrequest.use(noCache);
 
 //
 // Authentication
