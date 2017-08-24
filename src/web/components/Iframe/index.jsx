0bb2db99cb3ae7e3e3861e78b3e1436fd417652e
Cheton Wu <cheton@gmail.com>
2017-08-24 23:20:26 +0800

    Update Iframe component

--- src/web/components/Iframe/index.jsx
+++ src/web/components/Iframe/index.jsx
@@ -1,46 +1,81 @@
-import _ from 'lodash';
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
 import ReactDOM from 'react-dom';
 
-class Iframe extends React.Component {
+const mapSandboxFromObjectToString = (sandbox) => {
+    sandbox = { ...sandbox };
+
+    return Object.keys(sandbox)
+        .filter(key => sandbox[key])
+        .map(s => s.replace(/[A-Z]/g, '-$&').toLowerCase())
+        .join(' ');
+};
+
+class Iframe extends PureComponent {
     static propTypes = {
-        url: React.PropTypes.string.isRequired,
-        width: React.PropTypes.string,
-        height: React.PropTypes.string,
-        sandbox: React.PropTypes.string
+        src: PropTypes.string.isRequired,
+        width: PropTypes.string,
+        height: PropTypes.string,
+        sandbox: PropTypes.shape({
+            // Re-enables form submission
+            allowForms: PropTypes.bool,
+
+            // Sandboxed frames will block modal dialogs by default
+            allowModals: PropTypes.bool,
+
+            // Re-enables APIs
+            allowPointerLock: PropTypes.bool,
+
+            // Re-enables popups
+            allowPopups: PropTypes.bool,
+
+            // Allows the iframe content to be treated as being from the same origin
+            allowSameOrigin: PropTypes.bool,
+
+            // Re-enables scripts
+            allowScripts: PropTypes.bool,
+
+            // Allows the iframe content to navigate its top-level browsing context
+            allowTopNavigation: PropTypes.bool
+        })
     };
     static defaultProps = {
-        sandbox: 'allow-forms allow-same-origin allow-scripts',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        sandbox: {
+            allowForms: true,
+            allowModals: true,
+            allowPointerLock: false,
+            allowPopups: true,
+            allowSameOrigin: true,
+            allowScripts: true,
+            allowTopNavigation: false
+        }
     };
 
-    shouldComponentUpdate(nextProps, nextState) {
-        return !_.isEqual(nextProps, this.props);
-    }
     reload() {
-        let el = ReactDOM.findDOMNode(this);
-        el.src = this.props.url;
+        const el = ReactDOM.findDOMNode(this);
+        el.src = this.props.src;
     }
     render() {
-        const { url, width, height, ...others } = this.props;
-        const style = {
-            overflow: 'hidden',
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0
-        };
+        const { style, ...props } = this.props;
+        const sandbox = mapSandboxFromObjectToString(props.sandbox);
+        delete props.sandbox;
 
         return (
             <iframe
-                {...others}
                 frameBorder="0"
-                src={url}
-                style={style}
-                width={width}
-                height={height}
+                sandbox={sandbox}
+                style={{
+                    overflow: 'hidden',
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    ...style
+                }}
+                {...props}
             />
         );
     }
