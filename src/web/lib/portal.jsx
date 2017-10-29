ea67a7a12e64658d7e4bce61732044b77f62a5a5
Cheton Wu <cheton@gmail.com>
2017-10-29 10:42:08 +0800

    Provide an optional node argument for mounting component

--- src/web/lib/portal.jsx
+++ src/web/lib/portal.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default (Component) => new Promise((resolve, reject) => {
-    const container = document.createElement('div');
-    document.body.appendChild(container);
+export default (Component, node = null) => new Promise((resolve, reject) => {
+    let defaultNode = null;
+
+    if (!node) {
+        defaultNode = document.createElement('div');
+        defaultNode.setAttribute('data-portal', '');
+        document && document.body && document.body.appendChild(defaultNode);
+    }
 
     ReactDOM.render(
         <Component
             onClose={() => {
                 setTimeout(() => {
-                    ReactDOM.unmountComponentAtNode(container);
-                    container.remove();
+                    if (node) {
+                        ReactDOM.unmountComponentAtNode(node);
+                    } else if (defaultNode) {
+                        ReactDOM.unmountComponentAtNode(defaultNode);
+                        document && document.body && document.body.removeChild(defaultNode);
+                        defaultNode = null;
+                    }
+
                     resolve();
                 }, 0);
             }}
         />,
-        container
+        node || defaultNode
     );
 });
