147ea310b36cb5182537e7523a78f707f7cb1a79
Cheton Wu <cheton@gmail.com>
2017-11-13 16:25:19 +0800

    Add promise-series.js

---
+++ src/web/lib/promise-series.js
@@ -0,0 +1,8 @@
+const promiseSeries = (tasks, initialValue) => {
+    if (!Array.isArray(tasks)) {
+        return Promise.reject(new TypeError('"tasks" must be an array of functions'));
+    }
+    return tasks.reduce((p, task) => p.then(task), Promise.resolve(initialValue));
+};
+
+module.exports = promiseSeries;
