272c47290b38aaf9b08ad241c52dea9e09786743
Cheton Wu <cheton@gmail.com>
2017-12-02 09:58:22 +0800

    Remove unused codes

--- src/app/constants/index.js
+++ src/app/constants/index.js
@@ -1,24 +1,3 @@
-// IP_WHITELIST
-export const IP_WHITELIST = [
-    // IPv4 reserved space
-    '127.0.0.0/8', // Used for loopback addresses to the local host
-    '10.0.0.0/8', // Used for local communications within a private network
-    '172.16.0.0/12', // Used for local communications within a private network
-    '192.168.0.0/16', // Used for local communications within a private network
-    '169.254.0.0/16', // Link-local address
-
-    // IPv4 mapped IPv6 address
-    '::ffff:10.0.0.0/8',
-    '::ffff:127.0.0.0/8',
-    '::ffff:172.16.0.0/12',
-    '::ffff:192.168.0.0/16',
-
-    // IPv6 reserved space
-    '::1/128', // loopback address to the local host
-    'fc00::/7', // Unique local address
-    'fe80::/10' // Link-local address
-];
-
 // Error Codes
 export const ERR_BAD_REQUEST = 400;
 export const ERR_UNAUTHORIZED = 401;
