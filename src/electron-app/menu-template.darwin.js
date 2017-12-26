f77398ea564f1cc6fe4269c09eb59249d1b7c5a1
Cheton Wu <cheton@gmail.com>
2017-12-26 15:48:43 +0800

    [Electron] Refine the application menu

--- src/electron-app/menu-template.darwin.js
+++ src/electron-app/menu-template.darwin.js
@@ -1,8 +1,6 @@
 import { app, shell } from 'electron';
 import trimStart from 'lodash/trimStart';
 
-const appName = app.getName();
-
 // https://github.com/electron/electron/blob/master/docs/api/menu/
 export default (options) => {
     // routes = [
@@ -23,105 +21,54 @@ export default (options) => {
 
     const template = [
         {
-            label: appName,
+            label: app.getName(),
             submenu: [
+                { role: 'about' },
+                { type: 'separator' },
+                { role: 'services', submenu: [] },
+                { type: 'separator' },
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' },
+                { role: 'quit' }
+            ]
+        },
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' },
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                { role: 'pasteandmatchstyle' },
+                { role: 'delete' },
+                { role: 'selectall' },
+                { type: 'separator' },
                 {
-                    label: 'About ' + appName,
-                    role: 'about'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Services',
-                    role: 'services',
-                    submenu: []
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Hide ' + appName,
-                    accelerator: 'Command+H',
-                    role: 'hide'
-                },
-                {
-                    label: 'Hide Others',
-                    accelerator: 'Command+Alt+H',
-                    role: 'hideothers'
-                },
-                {
-                    label: 'Show All',
-                    role: 'unhide'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Quit',
-                    accelerator: 'Command+Q',
-                    click: () => {
-                        app.quit();
-                    }
+                    label: 'Speech',
+                    submenu: [
+                        { role: 'startspeaking' },
+                        { role: 'stopspeaking' }
+                    ]
                 }
             ]
         },
         {
             label: 'View',
             submenu: [
-                {
-                    label: 'Reload',
-                    accelerator: 'CmdOrCtrl+R',
-                    click: (item, focusedWindow) => {
-                        if (focusedWindow) {
-                            focusedWindow.reload();
-                        }
-                    }
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Toggle Full Screen',
-                    accelerator: 'Ctrl+Command+F',
-                    click: (item, focusedWindow) => {
-                        if (focusedWindow) {
-                            focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-                        }
-                    }
-                },
-                {
-                    label: 'Zoom',
-                    submenu: [
-                        {
-                            label: 'Zoom In',
-                            role: 'zoomin'
-                        },
-                        {
-                            label: 'Zoom Out',
-                            role: 'zoomout'
-                        },
-                        {
-                            label: 'Reset',
-                            role: 'resetzoom'
-                        }
-                    ]
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Toggle Developer Tools',
-                    accelerator: 'Alt+Command+I',
-                    click: (item, focusedWindow) => {
-                        if (focusedWindow) {
-                            focusedWindow.toggleDevTools();
-                        }
-                    }
-                },
-                {
-                    type: 'separator'
-                },
+                { role: 'reload' },
+                { role: 'forcereload' },
+                { role: 'toggledevtools' },
+                { type: 'separator' },
+                { role: 'resetzoom' },
+                { role: 'zoomin' },
+                { role: 'zoomout' },
+                { type: 'separator' },
+                { role: 'togglefullscreen' },
+                { type: 'separator' },
                 {
                     label: 'Home',
                     click: () => {
@@ -133,34 +80,20 @@ export default (options) => {
             ]
         },
         {
-            label: 'Window',
             role: 'window',
             submenu: [
-                {
-                    label: 'Minimize',
-                    accelerator: 'CmdOrCtrl+M',
-                    role: 'minimize'
-                },
-                {
-                    label: 'Close',
-                    accelerator: 'CmdOrCtrl+W',
-                    role: 'close'
-                },
-                {
-                    type: 'separator'
-                },
-                {
-                    label: 'Bring All to Front',
-                    role: 'front'
-                }
+                { role: 'close' },
+                { role: 'minimize' },
+                { role: 'zoom' },
+                { type: 'separator' },
+                { role: 'front' }
             ]
         },
         {
-            label: 'Help',
             role: 'help',
             submenu: [
                 {
-                    label: 'Help',
+                    label: 'Learn More',
                     click: () => {
                         shell.openExternal('https://github.com/cncjs/cncjs/wiki');
                     }
