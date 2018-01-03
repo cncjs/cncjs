08af76eccd79f785c4e4f4c572adafe6947f7d42
Cheton Wu <cheton@gmail.com>
2018-01-03 17:59:48 +0800

    Use CI build number for running the latest build

--- scripts/prebuild-latest.sh
+++ scripts/prebuild-latest.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rev=`git rev-list --count HEAD`
-
 mkdir -p dist
 rm -rf dist/*
 
 npm run package-sync
 
 pushd src
-npm version ${npm_package_version}-${rev}
+if [ ! -z "$CI_BUILD_NUMBER" ]; then
+    npm version ${npm_package_version}-${CI_BUILD_NUMBER};
+fi
 mkdir -p ../dist/cnc/
 cp -af package.json ../dist/cnc/
 babel -d ../dist/cnc/ *.js electron-app/**/*.js
