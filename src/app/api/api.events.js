6c2074fe26898196d49777f6dccd8bc2110ef592
Cheton Wu <cheton@gmail.com>
2018-07-06 01:14:14 +0800

    Addresses an issue that cannot create session files on a read-only file system (#319, #320, #347) (#348)
    * Rename cncrc to rcfile
    * Create session file path under user's home directory (#319, #320, #347)

--- src/app/api/api.events.js
+++ src/app/api/api.events.js
@@ -130,7 +130,7 @@ export const create = (req, res) => {
         res.send({ id: record.id, mtime: record.mtime });
     } catch (err) {
         res.status(ERR_INTERNAL_SERVER_ERROR).send({
-            msg: 'Failed to save ' + JSON.stringify(settings.cncrc)
+            msg: 'Failed to save ' + JSON.stringify(settings.rcfile)
         });
     }
 };
@@ -189,7 +189,7 @@ export const update = (req, res) => {
         res.send({ id: record.id, mtime: record.mtime });
     } catch (err) {
         res.status(ERR_INTERNAL_SERVER_ERROR).send({
-            msg: 'Failed to save ' + JSON.stringify(settings.cncrc)
+            msg: 'Failed to save ' + JSON.stringify(settings.rcfile)
         });
     }
 };
@@ -215,7 +215,7 @@ export const __delete = (req, res) => {
         res.send({ id: record.id });
     } catch (err) {
         res.status(ERR_INTERNAL_SERVER_ERROR).send({
-            msg: 'Failed to save ' + JSON.stringify(settings.cncrc)
+            msg: 'Failed to save ' + JSON.stringify(settings.rcfile)
         });
     }
 };
