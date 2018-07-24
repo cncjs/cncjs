9dc70224ce94f32e17e08d489daa2479743be3a0
Cheton Wu <cheton@gmail.com>
2018-07-24 23:44:13 +0800

    The closeOnOverlayClick prop was changed to disableOverlay

--- src/web/containers/Workspace/modals/FeederWait.jsx
+++ src/web/containers/Workspace/modals/FeederWait.jsx
@@ -10,7 +10,7 @@ import i18n from '../../../lib/i18n';
 const FeederWait = (props) => (
     <Modal
         size="xs"
-        closeOnOverlayClick={false}
+        disableOverlay={true}
         showCloseButton={false}
     >
         <Modal.Body>
