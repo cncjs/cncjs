3a18c88fac0c835a8fe01f02162b11b4c159b341
Cheton Wu <cheton@gmail.com>
2018-07-28 15:31:51 +0800

    Unit Test Improvement (#353)
    * Add exception tests
    
    * Add test for TinyG controller
    
    * eslint fixes
    
    * Add more tests

--- src/app/controllers/TinyG/TinyGRunner.js
+++ src/app/controllers/TinyG/TinyGRunner.js
@@ -160,7 +160,7 @@ class TinyGRunner extends events.EventEmitter {
 
                 this.emit('pwr', payload.pwr);
             } else if (type === TinyGLineParserResultQueueReports) {
-                const { qr } = payload;
+                const { qr, qi, qo } = payload;
 
                 // The planner buffer pool size will be checked every time the planner buffer changes
                 if (qr > this.plannerBufferPoolSize) {
@@ -173,7 +173,7 @@ class TinyGRunner extends events.EventEmitter {
                         qr
                     };
                 }
-                this.emit('qr', { qr });
+                this.emit('qr', { qr, qi, qo });
             } else if (type === TinyGLineParserResultStatusReports) {
                 // https://github.com/synthetos/TinyG/wiki/TinyG-Status-Codes#status-report-enumerations
                 const keymaps = {
