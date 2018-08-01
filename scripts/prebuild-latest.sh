253c367e0e8494900ca65aee492122d79dfdfab0
Cheton Wu <cheton@gmail.com>
2018-08-01 08:31:32 +0800

    Update package version with commits count rather than using CI build number

--- scripts/prebuild-latest.sh
+++ scripts/prebuild-latest.sh
@@ -4,11 +4,10 @@ mkdir -p dist
 rm -rf dist/*
 
 npm run package-sync
+commits_count=`git rev-list --count master`
 
 pushd src
-if [ ! -z "$CI_BUILD_NUMBER" ]; then
-    npm version ${npm_package_version}-${CI_BUILD_NUMBER};
-fi
+npm version ${npm_package_version}-${commits_count}
 mkdir -p ../dist/cnc/
 cp -af package.json ../dist/cnc/
 babel -d ../dist/cnc/ *.js electron-app/**/*.js
