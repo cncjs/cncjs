4dead81eed8b10959b30d79d2ffe1771bf825eff
Cheton Wu <cheton@gmail.com>
2019-01-21 19:21:18 +0800

    Refactor Spindle widget

--- src/app/widgets/Spindle/Spindle.jsx
+++ src/app/widgets/Spindle/Spindle.jsx
@@ -2,14 +2,20 @@ import cx from 'classnames';
 import ensureArray from 'ensure-array';
 import get from 'lodash/get';
 import PropTypes from 'prop-types';
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
+import { Button, ButtonGroup } from 'app/components/Buttons';
 import FontAwesomeIcon from 'app/components/FontAwesomeIcon';
+import FormGroup from 'app/components/FormGroup';
+import { FlexContainer, Row, Col } from 'app/components/GridSystem';
+import Label from 'app/components/Label';
 import Space from 'app/components/Space';
+import Input from 'app/components/FormControl/Input';
+import InputGroup from 'app/components/InputGroup';
 import controller from 'app/lib/controller';
 import i18n from 'app/lib/i18n';
 import styles from './index.styl';
 
-class Spindle extends PureComponent {
+class Spindle extends Component {
     static propTypes = {
         state: PropTypes.object,
         actions: PropTypes.object
@@ -18,24 +24,26 @@ class Spindle extends PureComponent {
     render() {
         const { state, actions } = this.props;
         const { canClick, spindleSpeed } = state;
-        //const spindle = get(state, 'controller.modal.spindle'); // FIXME
+        const spindle = get(state, 'controller.modal.spindle');
         const coolant = ensureArray(get(state, 'controller.modal.coolant'));
         const mistCoolant = coolant.indexOf('M7') >= 0;
         const floodCoolant = coolant.indexOf('M8') >= 0;
-        const spindle = 'M4'; // FIXME
 
         return (
-            <div>
-                <div className="form-group">
-                    <div className="row no-gutters">
-                        <div className="col-xs-6" style={{ paddingRight: 5 }}>
-                            <label className="control-label">{i18n._('Spindle')}</label>
-                            <div className="btn-group btn-group-justified" role="group">
-                                <div className="btn-group btn-group-sm" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-default"
-                                        style={{ padding: '5px 0' }}
+            <FlexContainer fluid>
+                <Row>
+                    <Col width={12}>
+                        <FormGroup>
+                            <Row>
+                                <Label>
+                                    {i18n._('Spindle control')}
+                                </Label>
+                            </Row>
+                            <Row>
+                                <ButtonGroup size="md">
+                                    <Button
+                                        btnStyle="default"
+                                        disabled={!canClick}
                                         onClick={() => {
                                             if (spindleSpeed > 0) {
                                                 controller.command('gcode', 'M3 S' + spindleSpeed);
@@ -44,18 +52,14 @@ class Spindle extends PureComponent {
                                             }
                                         }}
                                         title={i18n._('Spindle On, CW (M3)', { ns: 'gcode' })}
-                                        disabled={!canClick}
                                     >
-                                        <FontAwesomeIcon icon="sync-alt" spin={spindle === 'M3'} />
-                                        <Space width="4" />
+                                        <FontAwesomeIcon icon="redo-alt" spin={spindle === 'M3'} />
+                                        <Space width="8" />
                                         M3
-                                    </button>
-                                </div>
-                                <div className="btn-group btn-group-sm" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-default"
-                                        style={{ padding: '5px 0' }}
+                                    </Button>
+                                    <Button
+                                        btnStyle="default"
+                                        disabled={!canClick}
                                         onClick={() => {
                                             if (spindleSpeed > 0) {
                                                 controller.command('gcode', 'M4 S' + spindleSpeed);
@@ -64,42 +68,59 @@ class Spindle extends PureComponent {
                                             }
                                         }}
                                         title={i18n._('Spindle On, CCW (M4)', { ns: 'gcode' })}
-                                        disabled={!canClick}
                                     >
-                                        <FontAwesomeIcon icon="sync-alt" className={cx({ [styles.spinReverse]: spindle === 'M4' })} />
-                                        <Space width="4" />
+                                        <FontAwesomeIcon icon="undo-alt" className={cx({ [styles.spinReverse]: spindle === 'M4' })} />
+                                        <Space width="8" />
                                         M4
-                                    </button>
-                                </div>
-                                <div className="btn-group btn-group-sm" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-default"
-                                        style={{ padding: '5px 0' }}
+                                    </Button>
+                                    <Button
+                                        btnStyle="default"
+                                        disabled={!canClick}
                                         onClick={() => controller.command('gcode', 'M5')}
                                         title={i18n._('Spindle Off (M5)', { ns: 'gcode' })}
-                                        disabled={!canClick}
                                     >
                                         <FontAwesomeIcon icon="power-off" />
-                                        <Space width="4" />
+                                        <Space width="8" />
                                         M5
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-xs-6" style={{ paddingLeft: 5 }}>
-                            <label className="control-label">{i18n._('Coolant')}</label>
-                            <div className="btn-group btn-group-justified" role="group">
-                                <div className="btn-group btn-group-sm" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-default"
-                                        style={{ padding: '5px 0' }}
+                                    </Button>
+                                </ButtonGroup>
+                            </Row>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label>
+                                {i18n._('Spindle speed')}
+                            </Label>
+                            <InputGroup size="md">
+                                <Input
+                                    type="number"
+                                    value={spindleSpeed}
+                                    placeholder="0"
+                                    min={0}
+                                    step={1}
+                                    onChange={actions.handleSpindleSpeedChange}
+                                />
+                                <InputGroup.Append>
+                                    <InputGroup.Text>
+                                        {i18n._('RPM')}
+                                    </InputGroup.Text>
+                                </InputGroup.Append>
+                            </InputGroup>
+                        </FormGroup>
+                        <FormGroup>
+                            <Row>
+                                <Label>
+                                    {i18n._('Coolant control')}
+                                </Label>
+                            </Row>
+                            <Row>
+                                <ButtonGroup btnSize="md">
+                                    <Button
+                                        btnStyle="default"
+                                        disabled={!canClick}
                                         onClick={() => {
                                             controller.command('gcode', 'M7');
                                         }}
                                         title={i18n._('Mist Coolant On (M7)', { ns: 'gcode' })}
-                                        disabled={!canClick}
                                     >
                                         <i
                                             className={cx(
@@ -108,20 +129,16 @@ class Spindle extends PureComponent {
                                                 { 'fa-spin': mistCoolant }
                                             )}
                                         />
-                                        <Space width="4" />
+                                        <Space width="8" />
                                         M7
-                                    </button>
-                                </div>
-                                <div className="btn-group btn-group-sm" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-default"
-                                        style={{ padding: '5px 0' }}
+                                    </Button>
+                                    <Button
+                                        btnStyle="default"
+                                        disabled={!canClick}
                                         onClick={() => {
                                             controller.command('gcode', 'M8');
                                         }}
                                         title={i18n._('Flood Coolant On (M8)', { ns: 'gcode' })}
-                                        disabled={!canClick}
                                     >
                                         <i
                                             className={cx(
@@ -130,50 +147,27 @@ class Spindle extends PureComponent {
                                                 { 'fa-spin': floodCoolant }
                                             )}
                                         />
-                                        <Space width="4" />
+                                        <Space width="8" />
                                         M8
-                                    </button>
-                                </div>
-                                <div className="btn-group btn-group-sm" role="group">
-                                    <button
-                                        type="button"
-                                        className="btn btn-default"
-                                        style={{ padding: '5px 0' }}
+                                    </Button>
+                                    <Button
+                                        btnStyle="default"
+                                        disabled={!canClick}
                                         onClick={() => {
                                             controller.command('gcode', 'M9');
                                         }}
                                         title={i18n._('Coolant Off (M9)', { ns: 'gcode' })}
-                                        disabled={!canClick}
                                     >
                                         <FontAwesomeIcon icon="power-off" />
-                                        <Space width="4" />
+                                        <Space width="8" />
                                         M9
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="form-group">
-                    <div className="row no-gutters">
-                        <div className="col-xs-6" style={{ paddingRight: 5 }}>
-                            <label className="control-label">{i18n._('Spindle Speed')}</label>
-                            <div className="input-group input-group-sm">
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    value={spindleSpeed}
-                                    placeholder="0"
-                                    min={0}
-                                    step={1}
-                                    onChange={actions.handleSpindleSpeedChange}
-                                />
-                                <span className="input-group-addon">{i18n._('RPM')}</span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                                    </Button>
+                                </ButtonGroup>
+                            </Row>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </FlexContainer>
         );
     }
 }
