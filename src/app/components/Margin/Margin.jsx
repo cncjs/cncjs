85a2f63e46ef0830a06fa24bea6d6c8b7a031bd5
Cheton Wu <cheton@gmail.com>
2019-02-28 23:46:43 +0800

    Fixed a deprecation issue that `srcObject` should be used for a `MediaStream` object (#412)

--- src/app/components/Margin/Margin.jsx
+++ src/app/components/Margin/Margin.jsx
@@ -3,26 +3,26 @@ import React from 'react';
 
 const Margin = ({ style, ...props }) => {
     style = { ...style };
-    const { v = 0, h = 0, top = 0, right = 0, bottom = 0, left = 0, ...others } = { ...props };
+    const { vertical = 0, horizontal = 0, top = 0, right = 0, bottom = 0, left = 0, ...others } = { ...props };
 
-    if (v > 0) {
-        style.marginTop = v;
-        style.marginBottom = v;
+    if (vertical !== 0) {
+        style.marginTop = vertical;
+        style.marginBottom = vertical;
     }
-    if (h > 0) {
-        style.marginLeft = h;
-        style.marginRight = h;
+    if (horizontal !== 0) {
+        style.marginLeft = horizontal;
+        style.marginRight = horizontal;
     }
-    if (top > 0) {
+    if (top !== 0) {
         style.marginTop = top;
     }
-    if (right > 0) {
+    if (right !== 0) {
         style.marginRight = right;
     }
-    if (bottom > 0) {
+    if (bottom !== 0) {
         style.marginBottom = bottom;
     }
-    if (left > 0) {
+    if (left !== 0) {
         style.marginLeft = left;
     }
 
@@ -32,8 +32,8 @@ const Margin = ({ style, ...props }) => {
 };
 
 Margin.propTypes = {
-    v: PropTypes.number,
-    h: PropTypes.number,
+    vertical: PropTypes.number,
+    horizontal: PropTypes.number,
     top: PropTypes.number,
     right: PropTypes.number,
     bottom: PropTypes.number,
