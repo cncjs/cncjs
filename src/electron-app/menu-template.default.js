51234ef721e090673ea54174ff673ce0db3af55d
Cheton Wu <cheton@gmail.com>
2019-03-16 01:29:43 +0800

    Bump to 1.9.17 (#427)
    * Change "Home" to "View In Browser"
    
    * Update dependencies
    - electron@4.1.0
    - socket.io@2.2.0
    
    * Electron 4: Replace deprecated `app.makeSingleInstance` with `app.requestSingleInstanceLock`
    
    * Bump to 1.9.17
    
    * Change AppImage filename

--- src/electron-app/menu-template.default.js
+++ src/electron-app/menu-template.default.js
@@ -48,7 +48,7 @@ export default (options) => {
                 { role: 'togglefullscreen' },
                 { type: 'separator' },
                 {
-                    label: 'Home',
+                    label: 'View In Browser',
                     click: () => {
                         const url = `http://${address}:${port}`;
                         shell.openExternal(url);
