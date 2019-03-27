272d6ef44a76c3d85dcfe32bfd2a273f02a70a6a
Cheton Wu <cheton@gmail.com>
2019-03-27 19:24:59 +0800

    Add FormControl component

---
+++ src/web/components/FormControl/FormControl.jsx
@@ -0,0 +1,20 @@
+import cx from 'classnames';
+import React from 'react';
+import styles from './FormControl.styl';
+
+const FormControl = ({
+    tag: Component = 'div',
+    size,
+    className,
+    ...props
+}) => (
+    <Component
+        {...props}
+        className={cx(className, styles.formControl, {
+            [styles.formControlLg]: size === 'lg' || size === 'large',
+            [styles.formControlSm]: size === 'sm' || size === 'small',
+        })}
+    />
+);
+
+export default FormControl;
