0f43e74bd2931948a9d3921b977f4d1d78e25e4f
Cheton Wu <cheton@gmail.com>
2019-04-02 19:12:49 +0800

    eslint fix

--- src/web/widgets/Axes/DisplayPanel.jsx
+++ src/web/widgets/Axes/DisplayPanel.jsx
@@ -999,7 +999,6 @@ class DisplayPanel extends PureComponent {
     renderAxis = (axis) => {
         const { canClick, units, machinePosition, workPosition, jog } = this.props;
         const { actions } = this.props;
-        const wcs = actions.getWorkCoordinateSystem();
         const lengthUnits = (units === METRIC_UNITS) ? i18n._('mm') : i18n._('in');
         const degreeUnits = i18n._('deg');
         const mpos = machinePosition[axis] || '0.000';
