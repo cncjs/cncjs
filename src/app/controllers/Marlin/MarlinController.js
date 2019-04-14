e7e9f4601c719df7d6641838f5a5f2848e394951
Cheton Wu <cheton@gmail.com>
2019-04-14 23:21:23 +0800

    [g2core] Address an issue that spindle will restart after stopping a G-code program (#428)

--- src/app/controllers/Marlin/MarlinController.js
+++ src/app/controllers/Marlin/MarlinController.js
@@ -1069,7 +1069,7 @@ class MarlinController {
             },
             'stop': () => {
                 log.warn(`Warning: The "${cmd}" command is deprecated and will be removed in a future release.`);
-                this.command('gcode:stop');
+                this.command('gcode:stop', ...args);
             },
             // @param {object} options The options object.
             // @param {boolean} [options.force] Whether to force stop a G-code program. Defaults to false.
