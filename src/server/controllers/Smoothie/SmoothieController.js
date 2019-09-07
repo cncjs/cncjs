90bea48a28db11d399f2ee22a85bb3063a8102bc
Simon <fishpepper@gmail.com>
2019-09-07 19:52:57 +0200

    use proper suspend and resume commands, smoothie does not support !/~ properly

--- src/server/controllers/Smoothie/SmoothieController.js
+++ src/server/controllers/Smoothie/SmoothieController.js
@@ -1004,7 +1004,7 @@ class SmoothieController {
 
                 const activeState = _.get(this.state, 'status.activeState', '');
                 if (activeState === SMOOTHIE_ACTIVE_STATE_HOLD) {
-                    this.write('~'); // resume
+                    this.writeln('resume'); // resume
                 }
             },
             'pause': () => {
@@ -1016,7 +1016,7 @@ class SmoothieController {
 
                 this.workflow.pause();
 
-                this.write('!');
+                this.writeln('suspend');
             },
             'resume': () => {
                 log.warn(`Warning: The "${cmd}" command is deprecated and will be removed in a future release.`);
@@ -1025,7 +1025,7 @@ class SmoothieController {
             'gcode:resume': () => {
                 this.event.trigger('gcode:resume');
 
-                this.write('~');
+                this.writeln('resume');
 
                 this.workflow.resume();
             },
@@ -1037,7 +1037,7 @@ class SmoothieController {
                 if (this.workflow.state === WORKFLOW_STATE_RUNNING) {
                     return;
                 }
-                this.write('~');
+                this.writeln('resume');
                 this.feeder.unhold();
                 this.feeder.next();
             },
@@ -1047,12 +1047,12 @@ class SmoothieController {
             'feedhold': () => {
                 this.event.trigger('feedhold');
 
-                this.write('!');
+                this.writeln('suspend');
             },
             'cyclestart': () => {
                 this.event.trigger('cyclestart');
 
-                this.write('~');
+                this.writeln('resume');
             },
             'statusreport': () => {
                 this.write('?');
