d9a020c91c9c827911be261c14ddd22cf0089a57
Cheton Wu <cheton@gmail.com>
2019-10-05 22:27:29 +0800

    Fix eslint error for eact-hooks/exhaustive-deps

--- src/app/hooks/useUpdateEffect.js
+++ src/app/hooks/useUpdateEffect.js
@@ -1,16 +1,20 @@
 /* eslint consistent-return: 0 */
+import ensureArray from 'ensure-array';
 import { useEffect, useRef } from 'react';
 
 const useUpdateEffect = (effect, deps) => {
     const isInitialMount = useRef(true);
-
+    deps = ensureArray(deps);
     useEffect(() => {
         if (isInitialMount.current) {
             isInitialMount.current = false;
             return;
         }
 
-        return effect();
+        if (typeof effect === 'function') {
+            return effect();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, deps);
 };
 
