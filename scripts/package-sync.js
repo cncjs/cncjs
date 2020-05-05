8baaa725dd6fc4a92354d4406f976ff12df13e05
Cheton Wu <cheton@gmail.com>
2020-05-05 11:09:10 +0800

    chore: update package-sync script

--- scripts/package-sync.js
+++ scripts/package-sync.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 /* eslint max-len: 0 */
-const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
+const _pick = require('lodash/pick');
+const _uniq = require('lodash/uniq');
 const findImports = require('find-imports');
 
 // Copy necessary properties from 'package.json' to 'src/package.json'
@@ -14,11 +15,19 @@ const files = [
     'src/*.js',
     'src/server/**/*.{js,jsx}'
 ];
-const deps = [
+
+const resolvedImports = findImports(files, {
+    flatten: true,
+});
+
+const deps = _uniq([
     'core-js', // to polyfill ECMAScript features
     'regenerator-runtime', // needed to use transpiled generator functions
-    'debug' // 'debug' is required for electron app
-].concat(findImports(files, { flatten: true })).sort();
+    'debug', // 'debug' is required for electron app
+
+    // e.g. 'lodash/get' → 'lodash'
+    ...resolvedImports.map(x => x.split('/')[0]),
+]).sort();
 
 //pkgApp.name = pkg.name; // Exclude the name field
 pkgApp.version = pkg.version;
@@ -28,7 +37,7 @@ pkgApp.license = pkg.license;
 pkgApp.repository = pkg.repository;
 
 // Copy only Node.js dependencies to application package.json
-pkgApp.dependencies = _.pick(pkg.dependencies, deps);
+pkgApp.dependencies = _pick(pkg.dependencies, deps);
 
 const target = path.resolve(__dirname, '../src/package.json');
 const content = JSON.stringify(pkgApp, null, 2);
