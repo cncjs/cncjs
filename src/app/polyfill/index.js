9914b75e93d7ba88fbfde1b6554bb7311447c7ce
Cheton Wu <cheton@gmail.com>
2020-09-20 16:29:02 +0800

    chore: update deps and remove imports-loader

--- src/app/polyfill/index.js
+++ src/app/polyfill/index.js
@@ -2,5 +2,4 @@ import 'es5-shim/es5-shim';
 import 'es5-shim/es5-sham';
 import 'core-js/stable'; // to polyfill ECMAScript features
 import 'regenerator-runtime/runtime'; // needed to use transpiled generator functions
-import 'imports-loader?self=>window!js-polyfills/web'; // deps: Symbol
 import './console'; // IE9
