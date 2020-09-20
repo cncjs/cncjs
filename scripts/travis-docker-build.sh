955c35de77c9c4985af0e79b8479a857a0074938
Cheton Wu <cheton@gmail.com>
2020-09-20 22:06:54 +0800

    ci(docker): change a forward slash to another character for the tag name

--- scripts/travis-docker-build.sh
+++ scripts/travis-docker-build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-echo "TRAVIS_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER"
-echo "TRAVIS_COMMIT=$TRAVIS_COMMIT"
-echo "TRAVIS_TAG=$TRAVIS_TAG"
+# Change a forward slash to another character
+# - master       -> master
+# - feature/next -> feature-next
+# - v1.0         -> v1.0
+DOCKER_BRANCH_TAG=$(echo $TRAVIS_BRANCH | sed -e 's/\//-/g')
+echo "DOCKER_BRANCH_TAG=$DOCKER_BRANCH_TAG"
+
+# e.g. travis-1000.4cb69ef5
+DOCKER_BUILD_TAG=travis-$TRAVIS_BUILD_NUMBER.${TRAVIS_COMMIT::8}
+echo "DOCKER_BUILD_TAG=$DOCKER_BUILD_TAG"
 
 DOCKER_REPO=cncjs/cncjs
-DOCKER_BUILD_TAG=travis-$TRAVIS_BUILD_NUMBER.${TRAVIS_COMMIT::8}
-DOCKER_BRANCH_TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo -n "latest"; else echo -n "$TRAVIS_BRANCH"; fi`
-
 echo "DOCKER_REPO=$DOCKER_REPO"
-echo "DOCKER_BUILD_TAG=$DOCKER_BUILD_TAG"
-echo "DOCKER_BRANCH_TAG=$DOCKER_BRANCH_TAG"
+
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_BRANCH_TAG .
 #docker tag $DOCKER_REPO:$DOCKER_BUILD_TAG $DOCKER_REPO:$DOCKER_BRANCH_TAG
