a7df6796d124a4d16c7b4715b04e6c8522dd59a0
Cheton Wu <cheton@gmail.com>
2020-09-20 15:45:28 +0800

    chore: eslint fixes

--- src/app/hooks/useDeepCompareEffect.js
+++ src/app/hooks/useDeepCompareEffect.js
@@ -14,6 +14,7 @@ const useDeepCompareMemoize = (value) => {
 
 const useDeepCompareEffect = (callback, dependencies) => {
   dependencies = ensureArray(dependencies);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(callback, useDeepCompareMemoize(dependencies));
 };
 
--- src/app/hooks/useEffectOnce.js
+++ src/app/hooks/useEffectOnce.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
 const useEffectOnce = (effect) => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(effect, []);
 };
 
