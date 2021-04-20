90ba6af8fb6094303f76360d3bf8886eceee1c11
Cheton Wu <cheton@gmail.com>
2021-04-20 16:24:18 +0800

    chore: work in progress

--- scripts/ci-docker-build.sh
+++ scripts/ci-docker-build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-DOCKER_REPO=cncjs/cncjs
-DOCKER_BRANCH_TAG=`if [ "$CI_BRANCH" == "master" ]; then echo -n "latest"; else echo -n "$CI_BRANCH"; fi`
-
-echo "DOCKER_REPO=$DOCKER_REPO"
+# Change a forward slash to another character
+# - master       -> master
+# - feature/next -> feature-next
+# - v1.0         -> v1.0
+DOCKER_BRANCH_TAG=$(echo $CI_BRANCH | sed -e 's/\//-/g')
 echo "DOCKER_BRANCH_TAG=$DOCKER_BRANCH_TAG"
+
+DOCKER_REPO=cncjs/cncjs
+echo "DOCKER_REPO=$DOCKER_REPO"
+
 echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
 docker build -f Dockerfile -t $DOCKER_REPO:$DOCKER_BRANCH_TAG .
+
 docker images
 docker push $DOCKER_REPO
