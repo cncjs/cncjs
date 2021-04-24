01a8b81a4e629d95f0fc10f822ed984c460bfc0b
Cheton Wu <cheton@gmail.com>
2021-04-24 08:27:59 +0800

    chore: remove electron-rebuild from scripts/electron-builder.sh

--- scripts/electron-builder.sh
+++ scripts/electron-builder.sh
@@ -4,7 +4,7 @@ __dirname="$(CDPATH= cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 electron_version=$(electron --version)
 
 display_usage() {
-    npm run electron-builder -- --help
+    yarn electron-builder -- --help
 }
 
 if [ $# -le 1 ]; then
@@ -21,14 +21,15 @@ pushd "$__dirname/../dist/cncjs"
 echo "Cleaning up \"`pwd`/node_modules\""
 rm -rf node_modules
 echo "Installing packages..."
-npm install --production
-npm dedupe
+yarn --production
 popd
 
-echo "Rebuild native modules using electron ${electron_version}"
-npm run electron-rebuild -- \
-    --version=${electron_version:1} \
-    --module-dir=dist/cncjs \
-    --which-module=serialport
+#echo "Rebuild native modules using electron ${electron_version}"
+#npm run electron-rebuild -- \
+#    --version=${electron_version:1} \
+#    --module-dir=dist/cncjs \
+#    --which-module=serialport
 
-cross-env USE_HARD_LINKS=false npm run electron-builder -- "$@"
+yarn electron-builder install-app-deps
+
+cross-env USE_HARD_LINKS=false yarn electron-builder -- "$@"
