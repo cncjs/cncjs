d5d81a000eec73ffc0e78c6f06993554b0248860
Oleksiy Kovyrin <oleksiy@kovyrin.net>
2021-09-23 15:00:45 -0400

    Fix Marlin widget title

--- src/app/widgets/Marlin/Controller.jsx
+++ src/app/widgets/Marlin/Controller.jsx
@@ -16,7 +16,7 @@ const Controller = (props) => {
         <Modal disableOverlay size="lg" onClose={actions.closeModal}>
             <Modal.Header>
                 <Modal.Title>
-                    Grbl
+                    Marlin
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
