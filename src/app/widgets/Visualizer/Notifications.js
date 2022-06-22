b3c6120ec6238e7fd5af43e2d0b161b2c4c1ada4
Cheton Wu <cheton@gmail.com>
2022-06-22 14:00:55 +0800

    feat: display the G-code line of certain M commands (e.g. M0/M1/M6/...) in the toast notification (#768)

--- src/app/widgets/Visualizer/Notifications.js
+++ src/app/widgets/Visualizer/Notifications.js
@@ -44,36 +44,54 @@ const Notifications = ({ show, type, data, onDismiss, style, ...props }) => (
             {type === NOTIFICATION_PROGRAM_ERROR && (
                 <div>
                     <div><strong>{i18n._('Error')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Click the Resume button to resume program execution.')}</div>
                 </div>
             )}
             {type === NOTIFICATION_M0_PROGRAM_PAUSE && (
                 <div>
                     <div><strong>{i18n._('M0 Program Pause')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Click the Resume button to resume program execution.')}</div>
                 </div>
             )}
             {type === NOTIFICATION_M1_PROGRAM_PAUSE && (
                 <div>
                     <div><strong>{i18n._('M1 Program Pause')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Click the Resume button to resume program execution.')}</div>
                 </div>
             )}
             {type === NOTIFICATION_M2_PROGRAM_END && (
                 <div>
                     <div><strong>{i18n._('M2 Program End')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Click the Stop button to stop program execution.')}</div>
                 </div>
             )}
             {type === NOTIFICATION_M30_PROGRAM_END && (
                 <div>
                     <div><strong>{i18n._('M30 Program End')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Click the Stop button to stop program execution.')}</div>
                 </div>
             )}
             {type === NOTIFICATION_M6_TOOL_CHANGE && (
                 <div>
                     <div><strong>{i18n._('M6 Tool Change')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>
                         {i18n._('Run a tool change macro to change the tool and adjust the Z-axis offset. Afterwards, click the Resume button to resume program execution.')}
                         <Space width="4" />
@@ -89,12 +107,18 @@ const Notifications = ({ show, type, data, onDismiss, style, ...props }) => (
             {type === NOTIFICATION_M109_SET_EXTRUDER_TEMPERATURE && (
                 <div>
                     <div><strong>{i18n._('M109 Set Extruder Temperature')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Waiting for the target temperature to be reached...')}</div>
                 </div>
             )}
             {type === NOTIFICATION_M190_SET_HEATED_BED_TEMPERATURE && (
                 <div>
                     <div><strong>{i18n._('M190 Set Heated Bed Temperature')}</strong></div>
+                    {data && (
+                        <code>{data}</code>
+                    )}
                     <div>{i18n._('Waiting for the target temperature to be reached...')}</div>
                 </div>
             )}
