59b796e03dac68496dbd2fa4167f184d80909bad
khvalera <khvalera@ukr.net>
2023-07-31 00:26:12 +0300

    Ukrainian language update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
 export NODE_OPTIONS=--openssl-legacy-provider
+cd ..
+rm -Rf dist
+rm -f ~/.cncrc
+rm -fR ~/cncjs_i18n_old
+mkdir -p ~/cncjs_i18n_old/{app,server}
+
+cp -R src/app/i18n ~/cncjs_i18n_old/app
+cp -R src/server/i18n ~/cncjs_i18n_old/server
+
 yarn install
 yarn build-prod
-
 cd dist/cncjs/
 touch yarn.lock
 yarn install
