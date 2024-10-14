ca5ae0afd5db51b5ecea4f064d3fa95eedbd2bba
Cheton Wu <447801+cheton@users.noreply.github.com>
2024-10-14 23:31:22 +0800

    feat(Grbl): enhance Grbl parser to resolve issues with specific Grbl forks that generate incorrect parser state output ($G) (#883)
    * feat(Grbl): resolve issues with specific Grbl forks that generate incorrect parser state output ($G)
    
    * test: add a test case for handling invalid Grbl parser state
    
    * refactor: improve the pattern matching for parsing parser state output
    
    * test: update program state for a Grbl test case

--- src/server/controllers/Grbl/GrblLineParserResultParserState.js
+++ src/server/controllers/Grbl/GrblLineParserResultParserState.js
@@ -10,12 +10,37 @@ import {
 } from './constants';
 
 class GrblLineParserResultParserState {
-  // * Grbl v0.9
+  // * Grbl v0.9 output format:
+  //   ```
   //   [G38.2 G54 G17 G21 G91 G94 M0 M5 M9 T0 F20. S0.]
-  // * Grbl v1.1
+  //   ```
+  // * Grbl v1.x output format:
+  //   ```
   //   [GC:G0 G54 G17 G21 G90 G94 M0 M5 M9 T0 S0.0 F500.0]
+  //   ```
+  // * Some Grbl v1.x forks with incorrect parser state output:
+  //   ```
+  //   [GC:G0 G54 G17 G21 G90 G94 M5 M M9 T0 F0 S0]
+  //   [GC:G0 G54 G17 G21 G90 G94 M5 M9 T F0 S0]
+  //   ```
   static parse(line) {
-    const r = line.match(/^\[(?:GC:)?((?:[a-zA-Z][0-9]+(?:\.[0-9]*)?\s*)+)\]$/);
+    const parseLine = (line) => {
+      const patterns = [
+        // Matches standard output for Grbl v0.9 and v1.x
+        /^\[(?:GC:)?((?:[GMTFS]\d+(?:\.\d*)?\s+)+(?:[GMTFS]\d+(?:\.\d*)?))\]$/,
+        // Matches some Grbl v1.x forks with incorrect parser state output
+        /^\[GC:((?:[GMTFS]\d*(?:\.\d*)?\s+)+(?:[GMTFS]\d+(?:\.\d*)?))\]$/
+      ];
+      for (const pattern of patterns) {
+        const match = line.match(pattern);
+        if (match) {
+          return match;
+        }
+      }
+      return null;
+    };
+
+    const r = parseLine(line);
     if (!r) {
       return null;
     }
@@ -28,6 +53,11 @@ class GrblLineParserResultParserState {
 
     for (let i = 0; i < words.length; ++i) {
       const word = words[i];
+
+      if (word.length <= 1) {
+        // Skipping invalid fields like "M" or "T" without numeric values
+        continue;
+      }
 
       // Gx, Mx
       if (word.indexOf('G') === 0 || word.indexOf('M') === 0) {
