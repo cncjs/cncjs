b803289a790f46d641d284c4352d68628cf2c097
cheton <cheton@gmail.com>
2024-10-16 09:13:28 +0800

    feat: add a separate config file to store language options

---
+++ src/app/constants/i18n.js
@@ -0,0 +1,18 @@
+export const SUPPORTED_LANGUAGES = [
+  { value: 'de', label: 'Deutsch' },
+  { value: 'en', label: 'English (US)' },
+  { value: 'es', label: 'Español' },
+  { value: 'fr', label: 'Français' },
+  { value: 'it', label: 'Italiano' },
+  { value: 'hu', label: 'Magyar' },
+  { value: 'nb', label: 'Norwegian' },
+  { value: 'nl', label: 'Nederlands' },
+  { value: 'pt-br', label: 'Português (Brasil)' },
+  { value: 'pt-pt', label: 'Português (Portugal)' },
+  { value: 'ru', label: 'Русский' },
+  { value: 'tr', label: 'Türkçe' },
+  { value: 'uk', label: 'українська' },
+  { value: 'ja', label: '日本語' },
+  { value: 'zh-cn', label: '中文 (简体)' },
+  { value: 'zh-tw', label: '中文 (繁體)' }
+];
