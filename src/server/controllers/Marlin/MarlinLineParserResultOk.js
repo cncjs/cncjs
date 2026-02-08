f4bd40f9b8efbcb0ac467caf062deed8305f0a84
Wow <5232999+mAd-DaWg@users.noreply.github.com>
2026-02-09 01:34:39 +0200

    Update MarlinLineParserResultOk.js To fix Latest Marlin compatibility
    On the latest Marlin bufix-2.1.x, CNCjs widgets became unsuable do to the ok response from Marlin including feedback instead of just a `ok`. For example when Issueing a M105 command:
    
    ``` gcode
    M105 
    ok T:0
    S_XYZ:0
    ok P15 B3
    ```
    
    This fixes the widgets/server waiting indefinitely for a standalone `ok`, and wont break compatibility with older versions of marlin.

--- src/server/controllers/Marlin/MarlinLineParserResultOk.js
+++ src/server/controllers/Marlin/MarlinLineParserResultOk.js
@@ -1,7 +1,12 @@
 class MarlinLineParserResultOk {
   // ok
   static parse(line) {
-    const r = line.match(/^ok$/);
+    //Fix issues on latest marlin where responses can look like:
+    //M105
+    //ok T:0
+    //S_XYZ:0 
+    //ok P15 B3
+    const r = line.match(/^ok(\s|$)/i);
     if (!r) {
       return null;
     }
